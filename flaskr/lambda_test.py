e2a21044ea3fe55041400da9291a2296ad5b45f6
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2019-12-08 13:41:23 +0200

    update

--- flaskr/lambda_test.py
+++ flaskr/lambda_test.py
@@ -7,8 +7,9 @@ print( (lambda x, y: x + y)(2, 3) )
 def myfunc(n):
   return lambda a : a * n
 
+#int the function for n param
 mydoubler = myfunc(2)
-#print 20
+#print 20 init the lambda for a param
 print(mydoubler(10))
 #print 10
 print(f"mydoubler(5) = {mydoubler(5)}")
