39ba1321fd3c12b755b688f971592428d6de4888
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2019-12-11 07:36:09 +0200

    ud

--- flaskr/lambda_test.py
+++ flaskr/lambda_test.py
@@ -1,18 +1,14 @@
 import math
 
-
 def x(a, b, c): return a + b + c
-
 
 # print 6
 print(x(1, 2, 3))
 # print 5
 print((lambda x, y: x + y)(2, 3))
 
-
 def myfunc(n):
     return lambda a: a * n
-
 
 # int the function for n param
 mydoubler = myfunc(2)
@@ -21,14 +17,13 @@ print(mydoubler(10))
 # print 10
 print(f"mydoubler(5) = {mydoubler(5)}")
 
-#taylor serial to sin function
+# taylor serial to sin function
 def taylor_sin(angle: float):
     i: int
     sinus: float = 0
     sign: int = 1
     factorial: int = 1
-    power: float
-    power = angle
+    power: float = angle
     for i in range(10):
         sinus += sign * power / factorial
         sign = -sign
