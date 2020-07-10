a66e26214127c396dff7adfa0016452f5363c0cf
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2020-07-10 10:02:30 +0300

    fix errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -7,14 +7,14 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
 
   serverData: JSON;
   employeeData: JSON;
-  employee:JSON;
+  employee: JSON;
   name: string;
-  name2: string
+  name2: string;
 
   constructor(private httpClient: HttpClient) {
   }
@@ -26,22 +26,22 @@ export class AppComponent {
     this.httpClient.get('http://127.0.0.1:5000/').subscribe(data => {
       this.serverData = data as JSON;
       console.log(this.serverData);
-    })
+    });
   }
 
   getAllEmployees() {
-    this.httpClient.get('http://127.0.0.1:5000/employees').subscribe(data => {
-      this.names = data.employees.map(x => x.name)
-      //this.names = data.employees[0].name
+    this.httpClient.get('http://127.0.0.1:5000/employees').subscribe((data: any) => {
+      const names = data.employees.map(x => x.name);
+      // this.names = data.employees[0].name
       this.employeeData = data as JSON;
       console.log(this.employeeData);
-    })
+    });
   }
   getEmployee() {
-    this.httpClient.get('http://127.0.0.1:5000/employees/1').subscribe(data => {
-      this.name = data.data.name
+    this.httpClient.get('http://127.0.0.1:5000/employees/1').subscribe((data: any) => {
+      this.name = data.data.name;
       this.employee = data as JSON;
       console.log(this.employee);
-    })
+    });
   }
 }
