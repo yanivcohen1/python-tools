3549a125580edafe9e216c692271db4f0369d889
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2020-07-10 10:39:08 +0300

    read from get employee_id

--- flaskr/server.py
+++ flaskr/server.py
@@ -27,8 +27,8 @@ class Employees_Name(Resource):
         print('Employee id:' + employee_id)
         # parse x:
         # employees = json.loads(employees_json) # for convert json string to dictionary
-        firstEmployee = employees["employees"][0] #{'data': {'id':1, 'name':'Balram'}}
-        result = {'data': firstEmployee }
+        employee = employees["employees"][int(employee_id)] #{'data': {'id':1, 'name':'Balram'}}
+        result = {'data': employee }
         return jsonify(result)       
 
 
