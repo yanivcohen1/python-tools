be4d3cab9e6e7a344f3ec46e008bd428f770b93f
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2020-07-23 09:36:04 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,12 +37,14 @@ export class AppComponent implements OnInit {
       data.employees.forEach((x) => {
         this.names.push(x.name);
       });
+      console.log(this.names);
       // this.names = data.employees[0].name
       this.employeeData = data as JSON;
       console.log(this.employeeData);
       this.changeDetectorRef.detectChanges();
     });
   }
+
   getEmployee(val) {
     console.log(val + ` http://127.0.0.1:5000/employees/${val - 1}`);
     this.httpClient.get(`http://127.0.0.1:5000/employees/${val - 1}`).subscribe((data: any) => {
