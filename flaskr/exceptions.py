d9dd2f43a17e8f88f9725e684c88f1b4a0f0047c
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-05-11 16:46:35 +0300

    ud

--- flaskr/exceptions.py
+++ flaskr/exceptions.py
@@ -4,5 +4,6 @@ except Exception as inst:
     print("Error: " + inst.args[0])
 try:
     ser = 5/0
-except Exception as inst:
-    print("Error: " + inst.args[0])
+except Exception as err:
+    print("Error: " + err.args[0])
+    raise Exception(err.args[0])
