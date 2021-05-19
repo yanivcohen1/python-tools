d4dd74d2967bec11582a6d985bed0e364034e430
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-05-19 12:43:26 +0300

    new

---
+++ flaskr/runCmdTest.py
@@ -0,0 +1,6 @@
+import subprocess
+
+# result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE) # linux shell
+# result = subprocess.run(['cmd', '/c', 'dir'], stdout=subprocess.PIPE) # windows cmd
+result = subprocess.run(['powershell', '/c', 'ls'], stdout=subprocess.PIPE) # windows powershell
+print(result.stdout.decode('utf-8'))
