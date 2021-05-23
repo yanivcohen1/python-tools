9050b646dc40f1417dcd7cafba09d3aee7fb8a74
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-05-23 10:43:18 +0300

    new

---
+++ flaskr/typeof.py
@@ -0,0 +1,20 @@
+class Base:
+    pass
+
+class Derive(Base):
+    pass
+
+base = Base()
+print(type(base))
+# <class '__main__.Base'>
+derive = Derive()
+print(type(derive))
+# <class '__main__.Derive'>
+print(type(derive) is Derive)
+# True
+print(type(derive) is Base)
+# False
+print(isinstance(derive, Derive))
+# True
+print(isinstance(derive, Base))
+# True
