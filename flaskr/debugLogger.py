0404d5066a9e0308936b247b0e2f2d83f1cb2de3
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-06-22 11:20:46 +0300

    Add debug

---
+++ flaskr/debugLogger.py
@@ -0,0 +1,15 @@
+import logging, sys
+
+# print Debug & Info & WARNING & ERROR
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG) 
+# don't print debug only Info & WARNING & ERROR
+# logging.basicConfig(stream=sys.stderr, level=logging.INFO) 
+# don't print debug & Info only WARNING & ERROR
+#logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
+# print only ERROR
+#logging.basicConfig(stream=sys.stderr, level=logging.ERROR) 
+
+logging.debug('A debug message!')
+logging.info('A Info message processed %d records', 5)
+logging.warning('A warning message!')
+logging.error('A error message!')
