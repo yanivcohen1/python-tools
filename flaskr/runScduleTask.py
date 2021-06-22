c3f40f6a24d223cd869b7c403bae3dc23f7c2a84
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-06-22 09:38:00 +0300

    add schedule

---
+++ flaskr/runScduleTask.py
@@ -0,0 +1,24 @@
+import schedule
+import time
+from schedule import every, repeat
+
+@repeat(every(2).seconds, "every 2 sec")
+@repeat(every().day.at("10:30:42"), "every day at 10:30:42")
+def hello(planet):
+    print('This Annotation job run: ', planet)
+
+def greet(name):
+    print('This job run: ', name)
+
+schedule.every(3).seconds.do(greet, name='every 3 sec')
+# Run job every day at specific HH:MM and next HH:MM:SS
+schedule.every().day.at("10:30:42").do(greet, name='every day at 10:30:42')
+# If current time is 02:00, first execution is at 06:20:30
+schedule.every().wednesday.at("13:15").do(greet, name='every wednesday at 13:15')
+
+try :
+    while True :
+        schedule.run_pending()
+        time.sleep(1)
+except KeyboardInterrupt : # control+C press
+    pass
