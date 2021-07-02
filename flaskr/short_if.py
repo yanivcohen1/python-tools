fdc3de5bc061daad2214f04f637a64ca27e7f34b
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-02 08:09:01 +0300

    update

---
+++ flaskr/short_if.py
@@ -0,0 +1,14 @@
+a=2
+result = "is bigger" if a > 1 else "is smaller"
+print(result)
+
+a=True
+result = "is True" if a else "is False"
+print(result)
+
+a=2
+result = a+1 if a > 1 else "is smaller"
+print(result)
+a=0
+result = a+1 if a > 1 else "is smaller"
+print(result)
