2e340ba03d809569b1b5d7a4996b0657c987e8d9
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-06 00:24:59 +0300

    update

--- flaskr/short_if.py
+++ flaskr/short_if.py
@@ -9,9 +9,8 @@ print(result)
 a=2
 result = a+1 if a > 1 else "is smaller"
 print(result)
-a=0
-result = a+1 if a > 1 else "is smaller"
-print(result)
+
+if a==2: print("pass")
 
 # in javascript
 # var timeout = settings !== null ? settings.timeout : 1000;
