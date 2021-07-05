b42e939e29309619cbceb01ccd869e977d921fe3
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-06 00:39:56 +0300

    update

--- flaskr/short_if.py
+++ flaskr/short_if.py
@@ -2,18 +2,17 @@ a=2
 result = "is bigger" if a > 1 else "is smaller"
 print(result)
 
-a=True
-result = "is True" if a else "is False"
-print(result)
-
-a=2
 result = a+1 if a > 1 else "is smaller"
 print(result)
 
 if a==2: print("pass")
 
+print("pass1") if a==2 else print("pass2")
+
 # in javascript
 # var timeout = settings !== null ? settings.timeout : 1000;
+# (a==2) ? alert("please give me a lemonade") : alert("then give me a beer")
 
 # in java
 # Object bar = foo.isSelected() ? foo : baz;
+# (a==2) ? alert("please give me a lemonade") : alert("then give me a beer")
