e4d93b8b5440dea6eead359a14c2b18363f320f0
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-07 07:28:25 +0300

    update

--- flaskr/short_if.py
+++ flaskr/short_if.py
@@ -2,6 +2,7 @@
 a=2
 result = a+1 if a > 1 else "is smaller"
 print(result) # print 3
+print('Alarm is ' + ("ON" if a < 1 else "OFF"))
 
 # single line if else statment
 print("pass1") if a==2 else print("pass2") # print pass1
