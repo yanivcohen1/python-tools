26f8e804c40ddb39f946d46d9e8dd7cabeed4def
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-09 07:06:50 +0300

    update

--- flaskr/json_test.py
+++ flaskr/json_test.py
@@ -1,10 +1,18 @@
 import json
+# python dictionary
+z = {"age":30, "last":{"name": "cohen"}, "first":[{"name":"yaniv"}, {"age":30}]}
+# python parse to JSON staring
+x = json.dumps(z)
+print(x) # print '{"age":30, "last":{"name": "cohen"}, "first":[{"name":"yaniv"}, {"age":30}]}'
 
-# some JSON:
-x = '{"name":"John", "last":{"name": "cohen"}, "age":30, "first":[{"name":"yaniv"}, {"age":30}]}'
-# parse x:
+# python parse x to dictionary:
 y = json.loads(x)
 
 # the result is a Python dictionary:
 print(y["first"][0]["name"]) # print yaniv
 print(y["last"]["name"]) # print cohen
+
+# javascript parse JSON string to JSON
+# json = JSON.parse(JsonString)
+# javascript pars JSON to JSON string to send it
+# JsonString = JSON.stringify(json)
