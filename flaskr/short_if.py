0e2d9293303f0db445ed1e25b6bb9a1a321d60c8
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-14 17:16:29 +0300

    ud

--- flaskr/short_if.py
+++ flaskr/short_if.py
@@ -10,6 +10,11 @@ print("pass1") if a==2 else print("pass2") # print pass1
 # single line if statment
 if a==2: print("pass") # print pass
 
+arry = [1, 2, 3, 4, 5]
+# filter items and return multiply by 2
+filterArry = [2*item for item in arry if item % 2 == 1]
+print(filterArry) # print [2, 6, 10]
+
 # in javascript
 # var timeout = settings !== null ? settings.timeout : 1000; // single line if else assignment
 # (a==2) ? alert("please give me") : alert("then give me a beer") // single line if else statment
