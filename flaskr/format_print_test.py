a45a412af5213090ce69ab47b912db5dc52deabf
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-14 08:40:01 +0300

    new

---
+++ flaskr/format_print_test.py
@@ -0,0 +1,7 @@
+import math
+
+hith = 5
+radius = 10
+volume = int(math.pi * radius * radius * hith)
+print(f'The chlinder hith: {hith} and the radius: {radius} = \
+so the volume is:{int(math.pi * radius * radius * hith)} \n The End resoult need to be {volume}')
---
+++ flaskr/nested_fun_call.py
@@ -0,0 +1,12 @@
+import math
+def make_cylinder_volume_func(r):
+    def volume(h):
+        return math.pi * r * r * h
+    return volume
+
+radius = 10
+volume_radius_10 = make_cylinder_volume_func(radius) # input radius of the cylinder
+hith = 5
+volume = volume_radius_10(hith) # input hith of the cylinder
+
+print(f'Fhe chlinder hith: {hith} and the radius: {radius} = so the volume is:{int(volume)}')
