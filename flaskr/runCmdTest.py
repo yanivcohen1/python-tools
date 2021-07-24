1b5890d864c2673d45bca967d44b24b2bddb7dd8
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-24 12:49:32 +0300

    update

--- flaskr/runCmdTest.py
+++ flaskr/runCmdTest.py
@@ -2,5 +2,5 @@ import subprocess
 
 # result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE) # linux shell
 # result = subprocess.run(['cmd', '/c', 'dir'], stdout=subprocess.PIPE) # windows cmd
-result = subprocess.run(['powershell', '/c', 'ls'], stdout=subprocess.PIPE) # windows powershell
+result = subprocess.run(['powershell', '/c', 'cat', 'setup.cfg'], stdout=subprocess.PIPE) # windows powershell
 print(result.stdout.decode('utf-8'))
