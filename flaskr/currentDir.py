39164a7de87900f2fd5bd08134a22395b5389016
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-08-01 09:28:42 +0300

    ud

--- flaskr/currentDir.py
+++ flaskr/currentDir.py
@@ -1,4 +1,13 @@
 import os
+from pathlib import Path
 
 cwd = os.getcwd()
-print(cwd)
+print(cwd) # C:\Users\yaniv\OneDrive\python-flask
+
+path = Path('./')
+print(path.resolve()) # C:\Users\yaniv\OneDrive\python-flask
+new_path = path.resolve() / 'flaskr' / 'currentDir.py' 
+print(new_path.resolve()) # C:\Users\yaniv\OneDrive\python-flask\flaskr\currentDir.py
+print(new_path.exists()) # True
+print(new_path.is_dir()) # False
+with new_path.open() as f: print(f.readline()) # print - import os
