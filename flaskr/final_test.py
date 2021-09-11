89ee46e7034aad493bad37e61af326421b47833c
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-09-11 21:13:47 +0300

    update

--- flaskr/final_test.py
+++ flaskr/final_test.py
@@ -2,6 +2,8 @@ from typing import Final # in python 3.8 >=
 
 a: Final = 1.2
 b: float = 2
+c = (1,2,3) # tuple is read only
 # Executes fine, but mypy will report an error if you run mypy on this:
-# a = 2
+# a = 2 # editor error
+c[0] = 2 # runtime error
 print(a)
