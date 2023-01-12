932df5de28714ab921e08c43a179d308f42bce7a
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-01-12 09:04:37 +0200

    ud

--- flaskr/final_test.py
+++ flaskr/final_test.py
@@ -5,5 +5,5 @@ b: float = 2
 c = (1,2,3) # tuple is read only
 # Executes fine, but mypy will report an error if you run mypy on this:
 # a = 2 # editor error
-c[0] = 2 # runtime error
+c[0] = 2 # runtime error - tuple is read only
 print(a)
