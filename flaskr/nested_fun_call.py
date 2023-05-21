8c8c975d3e19f53d7c055717dbdbd7f71f2f917e
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-05-21 10:30:38 +0300

    ud

--- flaskr/nested_fun_call.py
+++ flaskr/nested_fun_call.py
@@ -1,7 +1,7 @@
 import math
-def make_cylinder_volume_func(r):
-    def volume(h):
-        return math.pi * r * r * h
+def make_cylinder_volume_func(_radius):
+    def volume(_hith):
+        return math.pi * _radius * _radius * _hith
     return volume
 
 radius = 10
