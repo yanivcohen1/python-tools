10fd5d02377b7cbe1a4b73535a621b2d37dba555
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-06-05 20:33:57 +0300

    ud

--- flaskr/Scapy/scapy_test.py
+++ flaskr/Scapy/scapy_test.py
@@ -1,20 +1,28 @@
 #  DNS להדפיס את כל הדומיינים שעבורם מתבצעת שאילתת
 from scapy.all import *
 
+googolIP = ""
+
 def print_query_name(dns_packet):
     """This function prints the domain name from a DNS query"""
-    print(dns_packet[DNSQR].qname)
+    print(dns_packet[DNSQR].qname, "--", dns_packet[DNS]["DNS Resource Record"].rdata)
 
 def filter_dns(packet):
     """This function filters query DNS"""
-    return (DNS in packet and packet[DNS].opcode == 0 and packet[DNSQR].qtype == 1)
+    if (DNS in packet and packet[DNS].opcode == 0 and packet[DNSQR].qtype == 1):
+        # packet.show()
+        global googolIP
+        googolIP = packet[DNS]["DNS Resource Record"].rdata
+        return True
+    else:
+        return False
 
 print("Sending to google 'Hello'")
-my_packet = IP(dst = "www.google.com") / Raw("Hello")
+my_packet = IP(dst = "www.google.com")/ TCP(dport = 80) / Raw("Hello")
 send(my_packet)
 
 print("Starting to sniff!")
-sniff(count=10, lfilter=filter_dns, prn=print_query_name)
+sniff(lfilter=filter_dns, prn=print_query_name, timeout=10)
 
 # -----------------------------------------------------------------
 
@@ -24,4 +32,11 @@ def is_good(packet):
 def print_packet(packet):
     print(packet[IP].src, "-", packet[IP].dst)
 
-sniff(count=3, lfilter=is_good, prn=print_packet)
+def stopfilter(x):
+    global googolIP
+    if IP in x and x[IP].src == googolIP:  # src_ip
+        return True
+    else:
+        return False
+
+sniff(lfilter=is_good, prn=print_packet, stop_filter=stopfilter, timeout=10)
