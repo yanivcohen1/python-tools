af7ae0b1276b01b54780733a602ab6bd80cfc742
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-06-09 15:49:39 +0300

    ud

--- flaskr/Scapy/scapy_arp_mac.py
+++ flaskr/Scapy/scapy_arp_mac.py
@@ -1,6 +1,24 @@
 #  DNS להדפיס את כל הדומיינים שעבורם מתבצעת שאילתת
 from scapy.all import *
 
+def findAllComputersInLocalNet():
+    computers = []
+    ips='192.168.1.0/24'
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp = ARP(pdst = ips)
+    answer, unanswered = srp(ether/arp, timeout = 2, inter = 0.1)
+    for sent, received in answer:
+        print(received.summary())
+        # received.show()
+        if received.haslayer(ARP):
+            ip_dict = {}
+            ip_dict["mac"] = received[ARP].hwsrc
+            ip_dict["ip"] = received[ARP].psrc
+        computers.append(ip_dict)
+    # for computer in computers:
+        # print(computer)
+    return computers
+
 # cap = sniff(count=15)
 def is_good(packet):
     if (packet.haslayer(ARP)):
@@ -9,6 +27,8 @@ def is_good(packet):
       return True
     else:
       return False
+
+computers_ip_mac = findAllComputersInLocalNet()
 
 # print the mac number
 cap = sniff(lfilter=is_good, timeout=15)
