2ab38b00d3bf51698b22f02cce172fd4639adb0b
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-06-10 13:00:11 +0300

    ud

--- flaskr/read_write_bin.py
+++ flaskr/read_write_bin.py
@@ -1,14 +1,14 @@
 import pickle
 
 # write list to binary file
-def write_list(a_list):
+def write_list(a_list, filename):
     # store list in binary file so 'wb' mode
-    with open('week29/packets.bin', 'wb') as fp:
+    with open(filename, 'wb') as fp:
         pickle.dump(a_list, fp)
 
 # Read list to memory
-def read_list():
+def read_list(fileName):
     # for reading also binary mode is important
-    with open('week29/packets.bin', 'rb') as fp:
+    with open(fileName, 'rb') as fp:
         n_list = pickle.load(fp)
         return n_list
