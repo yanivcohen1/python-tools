cea1617094a480fbc779f7f6aacafaad928a838b
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-06-12 19:22:01 +0300

    ud

--- flaskr/Scapy/scapy_dsn.py
+++ flaskr/Scapy/scapy_dsn.py
@@ -1,6 +1,24 @@
 #  DNS להדפיס את כל הדומיינים שעבורם מתבצעת שאילתת
 from scapy.all import *
 
+def getIP(domain):
+    SERVER_IP = "8.8.8.8"
+    DPORT = 53
+    fullmsg = IP(dst=SERVER_IP)/UDP(dport=DPORT)/DNS(rd=1,qd=DNSQR(qname=domain))
+    ans = sr1(fullmsg, verbose=0)
+    # print(ans.show())
+    print(domain + " -", ans["DNS"]["DNS Resource Record"].rdata)
+
+getIP("google.co.il")
+# ---------------BY SMIFF--------------------------------------------------
+
+def send_dns(domain):
+  #domain = "ynet.co.il"
+  SERVER_IP = "8.8.8.8"
+  DPORT = 53
+  fullmsg = IP(dst=SERVER_IP)/UDP(dport=DPORT)/DNS(rd=1,qd=DNSQR(qname=domain))
+  send(fullmsg)
+
 def print_query_name(dns_packet):
     """This function prints the domain name from a DNS query"""
     print(dns_packet[DNSQR].qname, "--", dns_packet[DNS]["DNS Resource Record"].rdata)
@@ -19,22 +37,7 @@ def filter_dns(packet):
     else:
         return False
 
-domain = "ynet.co.il"
-SERVER_IP = "8.8.8.8"
-DPORT = 53
-fullmsg = IP(dst=SERVER_IP)/UDP(dport=DPORT)/DNS(rd=1,qd=DNSQR(qname=domain))
-send(fullmsg)
-
+send_dns("ynet.co.il")
 # print the DNS
+print("start sniff 10 sec")
 sniff(lfilter=filter_dns, prn=print_query_name, timeout=10)
-
-# -----------------------------------------------------------------
-def getIP(domain):
-    SERVER_IP = "8.8.8.8"
-    DPORT = 53
-    fullmsg = IP(dst=SERVER_IP)/UDP(dport=DPORT)/DNS(rd=1,qd=DNSQR(qname=domain))
-    ans = sr1(fullmsg, verbose=0)
-    # print(ans.show())
-    print(domain + " -", ans["DNS"]["DNS Resource Record"].rdata)
-
-getIP("google.co.il")
