cabafcd6f8c5759498dd5bd568583cd9221f2848
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-06-24 16:23:40 +0300

    ud

--- flaskr/tools/get_post_html.py
+++ flaskr/tools/get_post_html.py
@@ -10,3 +10,14 @@ print("get:", response.text[:7])
 postdata = {"firstname": "John", "lastname": "Doe"} # params from dict
 resp = requests.post(URL, data=postdata) # post body params
 print('post:', resp.text[:7])
+
+# with socket
+import socket
+# get or post with socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("www.google.com", 80))
+# get for url about.html
+sock.send(b"GET /about.html HTTP/1.1\r\nHost:www.example.com\r\n\r\n")
+response = sock.recv(4096)
+sock.close()
+print(response.decode())
