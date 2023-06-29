bf9daab53a5f2fe1035bac162b4e5739dc4455a5
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-06-29 13:31:10 +0300

    ud

--- flaskr/runCmdTest.py
+++ flaskr/runCmdTest.py
@@ -1,6 +1,14 @@
 import subprocess
+import platform
 
 # result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE) # linux shell
 # result = subprocess.run(['cmd', '/c', 'dir'], stdout=subprocess.PIPE) # windows cmd
-result = subprocess.run(['powershell', '/c', 'cat', 'setup.cfg'], stdout=subprocess.PIPE) # windows powershell
+# result = subprocess.run(['powershell', '/c', 'cat', 'setup.cfg'], stdout=subprocess.PIPE) # windows powershell
+
+# for multiplatform run cmd.sh that contain #!/bin/bash ls -s"
+# need in linux to "chmod u+x script.sh"
+if  platform.system() != "Windows":
+    result = subprocess.run(['sh ./cmd.sh'], stdout=subprocess.PIPE, shell=True)
+else :
+    result = subprocess.run(['sh', './cmd.sh'], stdout=subprocess.PIPE, shell=True)
 print(result.stdout.decode('utf-8'))
