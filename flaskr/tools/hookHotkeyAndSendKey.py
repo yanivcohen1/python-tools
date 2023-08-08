a412256efea4a7b00851869e81c66401b8fda8ae
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-08-08 13:34:35 +0300

    ud

---
+++ flaskr/tools/hookHotkeyAndSendKey.py
@@ -0,0 +1,18 @@
+#  https://pypi.org/project/keyboard/
+# Import the keyboard module
+import keyboard
+
+# Define a function that sends a text when a hotkey is pressed
+def send_text():
+    # Use keyboard.write to type the text
+    keyboard.write("Hello, this is a text response")
+
+    # Use keyboard.press_and_release to press the enter key
+    keyboard.press_and_release("enter")
+
+# Use keyboard.add_hotkey to hook a hotkey to the function
+# For example, use ctrl+alt+t as the hotkey
+keyboard.add_hotkey("ctrl+alt+t", send_text)
+
+# Use keyboard.wait to keep the program running until esc is pressed
+keyboard.wait("esc")
