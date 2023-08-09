9e7c4370bda18cb22918a1028f83d02bb1f7c45d
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-08-09 12:01:54 +0300

    ud

--- flaskr/tools/addPathToModulesSearch.py
+++ flaskr/tools/addPathToModulesSearch.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append( os.getcwd() ) # where setup.py is
+sys.path.append( os.getcwd() ) # where setup.py is in "python-flask" dir
 from flaskr.lib.get_local_ip import get_local_ip
 
 print(get_local_ip(), os.getcwd())
