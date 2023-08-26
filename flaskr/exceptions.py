a237dd42232386adc578cf70478162d8e1ed474b
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-08-26 18:13:29 +0300

    ud

--- flaskr/exceptions.py
+++ flaskr/exceptions.py
@@ -18,9 +18,11 @@ print("********************")
 
 try:
     ser = 5/0
-    # raise Exception(str(err))
+    # raise Exception("can't divide by zero")
 except Exception as ex:
     print("'exceptions' module: Error (%s)." % str(ex))
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     print(exc_type, fname, exc_tb.tb_lineno)
+    # for forword the error
+    # raise Exception(str(ex))
