d26e79bf4c0216cfbc3325539b6e1229a774e65e
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-09-04 09:45:42 +0300

    ud

--- flaskr/helps.py
+++ flaskr/helps.py
@@ -1,9 +1,11 @@
 # print all evleble models
 help('modules')
 import os
+print("-------------str------------------")
+str(os.path)
+print("-------------help------------------")
 help(os)
 # help(module.funName)
 # help(class.funName)
 # obj = class()
 # str(obj)
-str(os)
