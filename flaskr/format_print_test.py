a03a3544e78aab440a5b83c0374f8490c54321b1
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-10-15 11:14:57 +0300

    ud

--- flaskr/format_print_test.py
+++ flaskr/format_print_test.py
@@ -21,7 +21,10 @@ print(f'\nThe class1 funcions and variables: {dir(class1)}')
 print(f'\nThe c1 funcions and variables: {dir(c1)}')
 print(f'\nThe c1 type: {type(c1)}')
 print(f'is c1 is type of class1: {isinstance(c1, class1)}')
+print(f'\n Th\\e c\'1 typ\\\'e: {type(c1)}') # Th\e c'1 typ\'e:
 
 print("can\"t") # can"t
 print("can\\\"t") # can\"t
 print("can\\t") # can\t
+
+
