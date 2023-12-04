47075ce93c9a7041e2e3a60de9ef5ac02a962560
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-12-04 23:01:42 +0200

    ud

--- flaskr/tools/req_get_post_html.py
+++ flaskr/tools/req_get_post_html.py
@@ -20,6 +20,18 @@ def post_json(URL_WithParams, json, headers):
     # print('post:', resp.text[:7])
     return resp.json()# return as json
 
+def send_rcv_cookies_and_download_file():
+    session = requests.Session()
+    url = 'http://google.com/favicon.ico'
+    cookies = {'cookies_are': 'working'}
+    myJson = {'somekey': 'somevalue'}
+    headers={"Content-Type":"json"}
+    response = session.post(url, allow_redirects=True, headers=headers, cookies=cookies, json = myJson)
+    open('google.ico', 'wb').write(response.content) # download file
+    print("print the cookies", session.cookies.get_dict())
+    print("print the headers", response.headers)
+    print("json of response", response.json)
+    print("text of response", response.text)
 
 if __name__ == '__main__':
     # get
