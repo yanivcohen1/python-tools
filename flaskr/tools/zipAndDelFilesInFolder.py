8611d6e5a2cc663d7dfa273663c6ff3e14214aa6
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-12-04 23:03:52 +0200

    ud

--- flaskr/tools/zipAndDelFilesInFolder.py
+++ flaskr/tools/zipAndDelFilesInFolder.py
@@ -1,19 +1,19 @@
 import os
 import shutil
 
-DOWMLOAD_DIR = "C:\\Temp\\Adi\\adi\\secend_year\\proj2\\download\\"
+DOWNLOAD_DIR =r"C:\Temp\Adi\adi\secend_year\proj2\download\\"
 ZIP_DIR = "C:\\Temp\\Adi\\adi\\secend_year\\proj2\\users\\"
 
 def create_zip_file_from_folder(zipName):
     """  Creating the user pics ZIP file
     """
     path = ZIP_DIR + zipName
-    archived = shutil.make_archive(path, 'zip', DOWMLOAD_DIR)
+    archived = shutil.make_archive(path, 'zip', DOWNLOAD_DIR)
 
     if os.path.exists(path+".zip"):
         print(archived)
         # delete all files in folder
-        for (dirpath, dirnames, filenames) in os.walk(DOWMLOAD_DIR):
+        for (dirpath, dirnames, filenames) in os.walk(DOWNLOAD_DIR):
             for filename in filenames:
                 os.remove(dirpath + "\\" + filename)
     else:
