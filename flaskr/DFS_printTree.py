599480918d35fbb3caaff3cdbe54403cd146d0d6
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-12-21 10:33:21 +0200

    ud

--- flaskr/DFS_printTree.py
+++ flaskr/DFS_printTree.py
@@ -1,3 +1,6 @@
+def print_res(ans, expcted):
+    print("" if ans == expcted else "         ", ans, "" if ans == expcted else "\nexpected:",
+            "" if ans == expcted else expcted)
 class TreeNode(object):
     def __init__(self, value, left=None, right=None):
         self.value = value
@@ -15,23 +18,42 @@ def printTree(node, level=0):
         printTree(node.left, level + 1)
         # print(' ' * 4 * level + '-> ' + str(node.value)) # print from bottom to top, print left first
 
+def returnTree(root, level=0):
+    res = []
+    stack = [root]
+    while stack:
+        node = stack.pop(0)
+        val = ""
+        if not node:
+            res.append(None)
+        else:
+            try: val = node.value
+            except: val = node.val
+            res.append(val)
+            if node.right or node.left:
+                stack.append(node.left)
+                stack.append(node.right)
+
+    return res
+
 class ListNode:
     def __init__(self, x, next=None):
         self.val = x
         self.next = next
 
 def printList(list: ListNode):
+    res = []
     while list:
-        print(list.val, end=" ")
+        res.append(list.val)
+        # print(list.val, end=" ")
         list = list.next
-    print()
+    # print()
+    return res
 
 if __name__ == '__main__':
-    tree = TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(7)),   TreeNode(9)),
-            TreeNode(3, TreeNode(5),  TreeNode(6)))
-    printTree(tree)
-    list = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
-    printList(list)
+    t = TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(7)),   TreeNode(9)),
+            TreeNode(3, TreeNode(5),    TreeNode(6)))
+    printTree(t)
 #             -> 7
 #         -> 4
 #     -> 2
@@ -40,3 +62,8 @@ if __name__ == '__main__':
 #         -> 5
 #     -> 3
 #         -> 6
+
+    print_res(returnTree(t), [1, 2, 3, 4, 9, 5, 6, 7, None])
+
+    t = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+    print_res(printList(t), [1, 2, 3, 4])
