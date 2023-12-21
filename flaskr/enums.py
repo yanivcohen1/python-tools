30ce4a562391d119871252441d450ef8dd40772d
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2023-12-21 13:27:55 +0200

    ud

--- flaskr/enums.py
+++ flaskr/enums.py
@@ -1,9 +1,22 @@
-from enum import Enum
+from enum import IntEnum
 # class syntax
-class Color(Enum):
+class Color(IntEnum):
     RED = 1
     GREEN = 2
     BLUE = 3
+    NOT_FOUND = -1
 
-print(Color.GREEN.name == "GREEN")
-print(Color.GREEN.value == 2)
+    @classmethod
+    def _missing_(cls, value):
+        return cls.NOT_FOUND
+
+print(Color.GREEN.name == "GREEN") # True
+print(Color.GREEN.value == 2) # True
+print(Color(5) == Color.NOT_FOUND) # True
+print(Color(5).name == "NOT_FOUND") # True
+
+enum_list = list(map(str, Color))
+print(enum_list) # prints ['Color.RED', 'Color.GREEN', 'Color.BLUE', 'Color.NOT_FOUND']
+
+enum_list = list(map(int, Color))
+print(enum_list) # prints [1, 2, 3, -1]
