3247062cdfe8ae77456d991366a259c1393a6978
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-02-01 13:28:39 +0200

    ud

--- flaskr/tools/selenium/auto_attach.py
+++ flaskr/tools/selenium/auto_attach.py
@@ -42,10 +42,9 @@ print(driver.session_id)
 # testing
 driver.maximize_window()
 # driver.find_element(By.CSS_SELECTOR, "a[href='https://www.neuralnine.com/books/']").click()
+# driver.find_element(By.PARTIAL_LINK_TEXT, "Books").click()
 links = driver.find_elements(By.XPATH, "//a[@href]")
 for link in links:
     if "Books" in link.get_attribute("innerHTML"):
         link.click()
         break
-
-By.PARTIAL_LINK_TEXT, "partial link text"
--- flaskr/tools/selenium/selenimue_auto.py
+++ flaskr/tools/selenium/selenimue_auto.py
@@ -12,6 +12,7 @@ driver = webdriver.Chrome(
 driver.get("https://www.neuralnine.com/")
 driver.maximize_window()
 # driver.find_element(By.CSS_SELECTOR, "a[href='https://www.neuralnine.com/books/']").click()
+# driver.find_element(By.PARTIAL_LINK_TEXT, "Books").click()
 links = driver.find_elements(By.XPATH, "//a[@href]")
 for link in links:
     if "Books" in link.get_attribute("innerHTML"):
