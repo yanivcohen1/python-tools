d7a6c26e9a81617ad3192bf931705304e8e4b726
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-02-01 09:00:19 +0200

    ud

--- flaskr/tools/selenium/auto_attach.py
+++ flaskr/tools/selenium/auto_attach.py
@@ -4,13 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import os
 # 1. open a driver
-# driver = webdriver.Chrome()  #python
-# options.browser_version = '92'
-# options.platform_name = 'Windows 10'
-# caps['version'] = '92'
-# caps['build'] = my_test_build
-# caps['name'] = my_test_name
-# options.set_capability(caps)
 # driver = webdriver.Chrome(options=options)
 # 2. extract to session_id and _url from driver object.
 # url = driver.command_executor._url       #"http://127.0.0.1:60622/hub"
@@ -19,9 +12,7 @@ import os
 options = Options()
 options.add_experimental_option("detach", True)
 url = "http://localhost:9515"
-# capabilities_options = UiAutomator2Options().load_capabilities(capabilities)
 driver = webdriver.Remote(command_executor=url, options=options)
-# this prevents the dummy browser
 session_id = ""
 # 4. And you are connected to your driver again.
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -34,6 +25,7 @@ try:
     # driver.get("https://www.neuralnine.com/")
     print(driver.current_url)
     driver.session_id = new_session_id
+    # this prevents the dummy browser
     driver.close()
     driver.session_id = session_id
 except:
