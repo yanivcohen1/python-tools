a3799a6bfd50a333184aa29b5b699de376291d13
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-02-01 16:03:19 +0200

    ud

---
+++ flaskr/tools/selenium/auto_attach_table.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import os
+
+# you need first to run the chrome driver in cmd (.\chromedriver.exe)
+
+# 1. open a driver
+# driver = webdriver.Chrome(options=options)
+# 2. extract to session_id and _url from driver object.
+# url = driver.command_executor._url       #"http://127.0.0.1:60622/hub"
+# session_id = driver.session_id            #'4e167f26-dc1d-4f51-a207-f761eaf73c31'
+# 3. Use these two parameter to connect to your driver.
+options = Options()
+options.add_experimental_option("detach", True)
+url = "http://localhost:9515"
+driver = webdriver.Remote(command_executor=url, options=options)
+session_id = ""
+# 4. And you are connected to your driver again.
+dir_path = os.path.dirname(os.path.realpath(__file__))
+FILE_NAME = dir_path + "\\session_id.txt"
+with open(FILE_NAME, "r") as f:
+    session_id = f.read()
+new_session_id = driver.session_id
+driver.session_id = session_id
+try:
+    # driver.get("https://www.neuralnine.com/")
+    print(driver.current_url)
+    driver.session_id = new_session_id
+    # this prevents the dummy browser
+    driver.close()
+    driver.session_id = session_id
+except:
+    driver.session_id = new_session_id
+    driver.get("https://www.w3schools.com/html/html_tables.asp")
+    with open(FILE_NAME, "w") as f:
+        f.write(driver.session_id)
+print(driver.session_id)
+
+# testing
+driver.maximize_window()
+# driver.find_element(By.CSS_SELECTOR, "a[href='https://www.neuralnine.com/books/']").click()
+# driver.find_element(By.PARTIAL_LINK_TEXT, "Books").click()
+table_id = driver.find_element(
+    By.CSS_SELECTOR,
+    "#customers",
+)
+# table_id = driver.find_element(By.ID, 'data_configuration_feeds_ct_fields_body0')
+rows = table_id.find_elements(By.TAG_NAME, "tr")  # get all of the rows in the table
+# head = rows[0].text.split(" ")
+head = 1
+for i, row in enumerate(rows):
+    if head:
+        cols = row.find_elements(By.TAG_NAME, "th")
+    else:
+        cols = row.find_elements(By.TAG_NAME, "td")
+    for col in cols:
+        print('%30s' % col.text, ",", end=" ")  # prints text from the element
+    if head:
+        print("\n----------------------------------------------------------------------------------------------------")
+        head = 0
+    else:
+        print()
