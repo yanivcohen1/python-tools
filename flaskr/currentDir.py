b17e871d211a66ad74939b9ed74c62205259471e
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-02-09 12:58:42 +0200

    ud

--- flaskr/currentDir.py
+++ flaskr/currentDir.py
@@ -6,8 +6,21 @@ print(cwd) # C:\Users\yaniv\OneDrive\python-flask
 
 path = Path('./')
 print(path.resolve()) # C:\Users\yaniv\OneDrive\python-flask
-new_path = path.resolve() / 'flaskr' / 'currentDir.py' 
+new_path = path.resolve() / 'flaskr' / 'currentDir.py'
 print(new_path.resolve()) # C:\Users\yaniv\OneDrive\python-flask\flaskr\currentDir.py
 print(new_path.exists()) # True
 print(new_path.is_dir()) # False
 with new_path.open() as f: print(f.readline()) # print - import os
+
+# ---------- recorcive on folder tree ------
+from os import walk
+
+f = []
+d = []
+mypath = os.getcwd()
+for (dirpath, dirnames, filenames) in walk(mypath):
+    f.extend(filenames) # all files in this dir
+    d.extend(filenames) # all directoris in this dir
+    break # only top level not recorsie - remove this for recorsive
+print("folders", f)
+print("\ndirectories", d)
--- flaskr/runCmdTest.py
+++ flaskr/runCmdTest.py
@@ -1,12 +1,14 @@
 import subprocess
 import platform
-
+import os
 # result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE) # linux shell
 # result = subprocess.run(['cmd', '/c', 'dir'], stdout=subprocess.PIPE) # windows cmd
 # result = subprocess.run(['powershell', '/c', 'cat', 'setup.cfg'], stdout=subprocess.PIPE) # windows powershell
 
 # for multiplatform run cmd.sh that contain #!/bin/bash ls -s"
 # need in linux to "chmod u+x script.sh"
+cwd = os.getcwd() # currect working dir
+print(cwd)
 if  platform.system() != "Windows":
     result = subprocess.run(['sh ./flaskr/cmd.sh'], stdout=subprocess.PIPE, shell=True)
 else :
