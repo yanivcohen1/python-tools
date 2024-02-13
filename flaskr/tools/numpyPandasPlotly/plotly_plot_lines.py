ab87aa1d9f5a5c26cc621f195a521da26fe647a3
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-02-13 04:03:24 +0200

    ud

--- flaskr/tools/numpyPandasPlotly/plotly_plot_lines.py
+++ flaskr/tools/numpyPandasPlotly/plotly_plot_lines.py
@@ -9,6 +9,37 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 # init_notebook_mode(connected=True)
 # Use Plotly locally
 # cf.go_offline()
+import pandas as pd
+
+csv = {
+  "Month": ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  "Sales": [1000, 1200, 1500, 1800, 2000, 2200],
+  "Profit": [200, 300, 400, 500, 600, 700],
+  'time': [1, 2, 3],
+}
+
+table = pd.DataFrame(csv, columns=['Month', 'Sales', "Profit"])# option to fillter
+# table = pd.read_csv('data.csv')
+
+# Panda print 2 first rows
+# use a list of indexes:
+print(table.loc[[0, 1]])
+#    Month  Sales  Profit
+# 0   Jan   1000     200
+# 1   Feb   1200     300
+# Make multiple line plots
+fig = px.line(table, x='Month', y=['Sales','Profit'], # select two columes for two lines
+        # labels={'x':'Date', 'y':'Value of Dollar'}, # not working take it from the colume name
+        title='Sales Vs. Profit', markers=True)
+
+fig.update_layout(
+    yaxis_title="Y Axis Title",
+    legend_title="Legend Title")
+
+fig.show()
+
+
+# -------------- simple line --------
 
 # Plot the value of a dollar invested over time
 # Use included Google price data to make one plot
