4b092935c01205e0e035fca1985b1d92c82ee9dc
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-02-13 04:49:10 +0200

    ud

--- flaskr/tools/numpyPandasPlotly/plotly_lines_express_panda.py
+++ flaskr/tools/numpyPandasPlotly/plotly_lines_express_panda.py
@@ -6,19 +6,22 @@ import plotly.graph_objects as go
 
 # Make Plotly work in your Jupyter Notebook
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
 # init_notebook_mode(connected=True)
 # Use Plotly locally
 # cf.go_offline()
 import pandas as pd
 
+pd.options.plotting.backend = "plotly"
+
 csv = {
-  "Month": ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-  "Sales": [1000, 1200, 1500, 1800, 2000, 2200],
-  "Profit": [200, 300, 400, 500, 600, 700],
-  'time': [1, 2, 3],
+    "Month": ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    "Sales": [1000, 1200, 1500, 1800, 2000, 2200],
+    "Profit": [200, 300, 400, 500, 600, 700],
+    "time": [1, 2, 3],
 }
 
-table = pd.DataFrame(csv, columns=['Month', 'Sales', "Profit"])# option to fillter
+table = pd.DataFrame(csv, columns=["Month", "Sales", "Profit"])  # option to fillter
 # table = pd.read_csv('data.csv')
 
 # Panda print 2 first rows
@@ -29,15 +32,31 @@ print(table.loc[[0, 1]])
 # 1   Feb   1200     300
 
 # Make multiple line plots
-fig = px.line(table, x='Month', y=['Sales','Profit'], # select two columes for two lines
-        # labels={'x':'Date', 'y':'Value of Dollar'}, # not working take it from the colume name
-        title='Sales Vs. Profit', markers=True)
+fig = px.line(
+    table,
+    x="Month",
+    y=["Sales", "Profit"],  # select two columes for two lines
+    # labels={'x':'Date', 'y':'Value of Dollar'}, # not working take it from the colume name
+    title="Sales Vs. Profit",
+    markers=True,
+)
 
-fig.update_layout(
-    yaxis_title="Y Axis Title",
-    legend_title="Legend Title")
+fig.update_layout(yaxis_title="Y Axis Title", legend_title="Legend Title")
 
 fig.show()
+
+# -------------- using panda for plot -----------------
+
+# plot a line plot of the Sales column with points
+# fig = go.Figure()
+fig: go.Figure = table.plot.line(
+    x="Month", y=["Sales", "Profit"], title="plot for panda data", markers=True
+)
+
+fig.update_layout(yaxis_title="Y Axis Title", legend_title="Legend Title")
+
+fig.show()
+
 
 
 # -------------- simple line --------
