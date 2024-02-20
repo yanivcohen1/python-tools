85b5f5e1f6e592fd7384db459ba29ab66c342fa8
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-02-20 09:52:47 +0200

    ud

--- flaskr/tools/numpyPandasPlotly/plotly_Subplots.py
+++ flaskr/tools/numpyPandasPlotly/plotly_Subplots.py
@@ -9,9 +9,9 @@ fig = make_subplots(
 # Add traces
 fig.add_trace(go.Scatter(x=[1, 2, 3], y=[4, 5, 6], name="plot 1.1"), row=1, col=1)
 fig.add_trace(go.Scatter(x=[2, 3, 4], y=[4.5, 5.5, 7.0], name="plot 1.2"), row=1, col=1)
-fig.add_trace(go.Scatter(x=[20, 30, 40], y=[50, 60, 70], name="plot 2"), row=1, col=2)
-fig.add_trace(go.Scatter(x=[300, 400, 500], y=[600, 700, 800], name="plot 3"), row=2, col=1)
-fig.add_trace(go.Scatter(x=[4000, 5000, 6000], y=[7000, 8000, 9000], name="plot 4"), row=2, col=2)
+fig.add_trace(go.Scatter(x=[20, 30, 40], y=[50, 60, 70], name="plot 2.1"), row=1, col=2)
+fig.add_trace(go.Scatter(x=[300, 400, 500], y=[600, 700, 800], name="plot 3.1"), row=2, col=1)
+fig.add_trace(go.Scatter(x=[4000, 5000, 6000], y=[7000, 8000, 9000], name="plot 4.1"), row=2, col=2)
 
 # Update xaxis properties
 fig.update_xaxes(title_text="xaxis 1 title", row=1, col=1)
