bfccef72d9332439ec1f6b44c929b614c6dd2624
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-09 08:03:51 +0200

    ud

--- flaskr/tools/openInExternalProg.py
+++ flaskr/tools/openInExternalProg.py
@@ -3,3 +3,4 @@ from tkinter import filedialog
 
 fileName = filedialog.askopenfilename()
 subprocess.run(['cmd', '/c', "C:\ProgramFiles\HxD\HxD.exe", fileName], stdout=subprocess.PIPE, shell=True)
+subprocess.run(['cmd', '/c', fileName ], stdout=subprocess.PIPE, shell=True)
