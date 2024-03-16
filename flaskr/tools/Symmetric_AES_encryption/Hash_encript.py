c9a2cba7eb59d25ccb0c6c5e158e8af4910560be
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-16 20:23:57 +0200

    ud

--- flaskr/tools/Symmetric_AES_encryption/Hash_encript.py
+++ flaskr/tools/Symmetric_AES_encryption/Hash_encript.py
@@ -4,7 +4,8 @@ str1 = 'First'
 hash256 = SHA256.new(data=str1.encode())
 # hash256.update('message')
 digest = hash256.digest()
-print(digest)
+print("bin digest", digest)
+print("hex digest", hash256.hexdigest())
 
 hash256_2 = SHA256.new(data=str1.encode())
 print("is msg not change: ", digest == hash256_2.digest())
