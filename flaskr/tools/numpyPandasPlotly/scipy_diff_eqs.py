4b9bbadfe601d603fb3934638bbd851d33f0580e
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-20 14:49:29 +0200

    ud

---
+++ flaskr/tools/numpyPandasPlotly/scipy_diff_eqs.py
@@ -0,0 +1,29 @@
+# dT2t = dT^2/dt = -(3.083e8*np.exp(-56000/(8.314*T0))*dT*0.033)
+# dT/dt = (0.45*-98000*dT2t+5.7431*(273.15-T0))/(2018.94)
+# My initial values are:
+# T[0]= T0 = 281.15
+# T'[0]= Td0 = 6.529
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+t = np.linspace(0, 1800, 901)    # timeline
+c0 = np.array([281.15, 6.529])  # initial values
+
+def df(c, t):
+    Temp = c[0]
+    dT = c[1]
+    Tdt = -(3.083e8*np.exp(-56000/(8.314*Temp))*dT*0.033)
+    dTdt = (0.45*-98000*Tdt + 5.7431*(273.15 - Temp))/(2018.94)
+    return np.array([dTdt, Tdt])
+
+sol = odeint(df, c0, t)
+plt.plot(t, sol[:,0] - 273.15, label='T')
+plt.plot(t, sol[:,1], label='T\'')
+plt.xlabel('t (sec)')
+plt.ylabel('T (degrees C)')
+plt.grid()
+plt.legend()
+plt.title("Temperature over time")
+plt.show()
