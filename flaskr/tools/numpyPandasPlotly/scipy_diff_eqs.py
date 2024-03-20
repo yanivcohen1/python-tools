d7d10d99a48b0d07dc99e90fb783f8a7200650da
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-20 17:25:22 +0200

    ud

--- flaskr/tools/numpyPandasPlotly/scipy_diff_eqs.py
+++ flaskr/tools/numpyPandasPlotly/scipy_diff_eqs.py
@@ -1,3 +1,5 @@
+# https://apmonitor.com/pdc/index.php/Main/SolveDifferentialEquations
+
 # T: Tempurter, t: time
 # dT2t = dT^2/dt = -(3.083e8*np.exp(-56000/(8.314*T0))*dTt*0.033)
 # dTt = dT/dt = (0.45*-98000*dT2t+5.7431*(273.15-T0))/(2018.94)
