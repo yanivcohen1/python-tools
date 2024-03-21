fb0335a1225017f2d8309224b4458994729b2901
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-21 15:39:19 +0200

    ud

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
      "flask-socketio==5.0.1", "schedule==1.1.0", "Jinja2==2.11.3", "markupsafe==1.1.1", "werkzeug==1.0.1", "pynput==1.7.6",
      "scapy", "psutil", "keyboard", "sortedcontainers", "numpy", "matplotlib", "pandas", "pandas_datareader",
      "plotly", "cufflinks", "ipykernel", "selenium", "webdriver-manager", "sympy", "scipy", "pycryptodome",
-     "ipywidgets", "IPython", "seaborn "],
+     "ipywidgets", "IPython", "seaborn", "ipykernel", "nbformat"],
     extras_require={"test": ["pytest", "coverage"]},
 )
