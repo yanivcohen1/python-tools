f7031b7be7da4176bbd4f8223e64dfd70bfedf5d
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-23 07:22:41 +0200

    ud

--- flaskr/tools/numpyPandasPlotly/scipy_eqs.py
+++ flaskr/tools/numpyPandasPlotly/scipy_eqs.py
@@ -1,6 +1,7 @@
 # f(x, y) = x² + y² - 10.0 = 0
-# g(x, y) = x + y + k = 0 ; for 1<= k <=3
-# You should get x = 0.1583124, y = -3.1583124
+# g(x, y) = x + y + k = 0 ; for k: 0 <= k <= 4
+# You should get x = 0.1583124, y = -3.1583124, whene k = 3
+# for k=0 you should get:  x = 2.23606798, y = -2.23606798
 # if you start with x = y = 1 as an initial guess.
 from scipy.optimize import fsolve
 
@@ -12,7 +13,7 @@ def h(z, k):
     return [f,g]
 
 z0 = [1,1]
-for k in [1,2,3]:
+for k in range(0, 5, 2):
     print(f"for k={k}: ", fsolve(h, z0, args=(k)))
 # the output is [x_root, y_root]:
 # for k=1:  [-2.67944947  1.67944947]
