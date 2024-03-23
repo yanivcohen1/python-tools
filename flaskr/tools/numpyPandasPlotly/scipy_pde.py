67a6c59126ce2233b54f244c74ef6b030cb9acfe
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-23 21:34:55 +0200

    ud

--- flaskr/tools/numpyPandasPlotly/scipy_pde.py
+++ flaskr/tools/numpyPandasPlotly/scipy_pde.py
@@ -2,19 +2,20 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-# Define the PDE as a system of ODEs using the method of lines
-def pde_system(t, y, N):
-    dydt = np.zeros_like(y)
-    dydt[1:-1] = (y[:-2] - 2*y[1:-1] + y[2:]) / dx**2  # Discretized Laplacian
-    return dydt
+# from mpl_toolkits.mplot3d import Axes3D
 
 # Set up initial conditions and parameters
 N = 100  # Number of spatial points
 x = np.linspace(0, 1, N)
-dx = x[1] - x[0]
+dx = x[1] - x[0] # (1-0)/N
 y0 = np.sin(np.pi * x)  # Initial condition
+
+# Define the PDE as a system of ODEs using the method of lines
+def pde_system(t, y, N):
+    dydt = np.zeros_like(y)
+    # Discretized Laplacian | x=x_i: u_{i-1}-2u_i+u_{i+1} / dx^2
+    dydt[1:-1] = (y[:-2] - 2*y[1:-1] + y[2:]) / dx**2  # Discretized Laplacian
+    return dydt
 
 # Solve the PDE
 solution = solve_ivp(lambda t, y: pde_system(t, y, N), [0, 1], y0, method='RK45')
@@ -28,7 +29,8 @@ X, T = np.meshgrid(x, solution.t)
 ax.plot_surface(X, T, solution.y.T, cmap='viridis')
 
 # Set labels and show the plot
-ax.set_xlabel('Spatial coordinate')
+ax.set_xlabel('Position - Spatial coordinate')
 ax.set_ylabel('Time')
 ax.set_zlabel('Temperature')
+plt.title("Temperature U(x,t) by solve PDE Heat Equation: ∂u/∂t ​= κ*∂^2*u/∂x^2")
 plt.show()
