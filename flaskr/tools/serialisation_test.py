ff733683963953f3ad5c838db7a6579831fa4e7e
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-29 10:41:04 +0300

    ud

---
+++ flaskr/tools/serialisation_test.py
@@ -0,0 +1,27 @@
+import pickle
+import json
+
+# Create a variable
+myvar = [{'This': 'is', 'Example': 1}, 'of',
+          'serialisation', ['using', 'pickle']]
+
+print("To serialized msg:", myvar)
+# Use dumps() to make it serialized
+serialized = pickle.dumps(myvar)
+print("serialized msg:", serialized)
+
+unSerialized = pickle.loads(serialized)
+print("UnSerialized msg:", unSerialized)
+
+# -------------------------------------------------------
+# serialized to text by json
+myvar = [{'This': 'is', 'Example': 1}, 'of',
+          'serialisation', ['using', 'pickle']]
+
+print("\nJson obj to json str:", myvar)
+# Use dumps() to make it serialized
+json_str = json.dumps(myvar)
+print("json str msg:", json_str)
+
+json_obj = json.loads(json_str)
+print("Form json str to json obj:", json_obj)
