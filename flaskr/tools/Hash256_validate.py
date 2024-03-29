b576aa00513e680a6d5f5c3340da8d371c9ca9a8
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-03-29 15:07:45 +0300

    ud

--- flaskr/tools/Hash256_validate.py
+++ flaskr/tools/Hash256_validate.py
@@ -7,11 +7,11 @@ digest = hash256.digest()
 print("bin digest:", digest)
 print("hex digest:", hash256.hexdigest())
 
-hash256_2 = SHA256.new(data=msg.encode())
+hash256_2 = SHA256.new(msg.encode())
 print("is msg not change: ", digest == hash256_2.digest())
 
 # --------- test from const--------------------
-hash256 = SHA256.new(data=b'yaniv')
+hash256 = SHA256.new(b'yaniv')
 digest = hash256.hexdigest()
 print("\nhex digest calc:", digest)
 hexToValidate = "c7b18ad0447f947c2a4304951a624a599f61d66e1b62c36a43e4a4091e4e11c6"
