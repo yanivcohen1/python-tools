dfb4a5a6d5299334232dcf65ba8727a943a25e47
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-02 11:58:55 +0300

    ud

--- flaskr/tools/gumini_test.py
+++ flaskr/tools/gumini_test.py
@@ -1,21 +1,16 @@
 # https://ai.google.dev/tutorials/python_quickstart
 import pathlib
 import textwrap
-import os
-from dotenv import load_dotenv
-from pathlib import Path
 import configparser
 
 config = configparser.ConfigParser()
 secsecss = config.read('flaskr/tools/env_local.ini')
 GEMINI_API_KEY = config['DEFAULT']['GEMINI_API_KEY']
-# dotenv_path = Path('yaniv/.env.local')
-# load_dotenv(dotenv_path=dotenv_path)
-
 # GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 import google.generativeai as genai
 
+# for jupiter
 # from IPython.display import display
 # from IPython.display import Markdown
 
@@ -24,12 +19,6 @@ import google.generativeai as genai
 #     text = text.replace('•', '  *')
 #     return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-
-# Used to securely store your API key
-# from google.colab import userdata
-
-# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
-# GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
 genai.configure(api_key=GEMINI_API_KEY)
 
 
@@ -42,8 +31,8 @@ model = genai.GenerativeModel('gemini-pro')
 # print(response.text)
 # # print(response.candidates)
 
-# print("test pass")
 while True:
     query = input("enter query:")
     response = model.generate_content(query)
-    print(response.text)
+    try: print(response.text)
+    except Exception as e: pass
