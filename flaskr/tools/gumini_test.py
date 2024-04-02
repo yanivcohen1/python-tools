99184e36bb460c15f0169ea8c4411ee704da4213
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-02 11:59:26 +0300

    ud

--- flaskr/tools/gumini_test.py
+++ flaskr/tools/gumini_test.py
@@ -14,7 +14,6 @@ import google.generativeai as genai
 # from IPython.display import display
 # from IPython.display import Markdown
 
-
 # def to_markdown(text):
 #     text = text.replace('•', '  *')
 #     return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
