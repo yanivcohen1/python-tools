8257eb4c0661c343948cae991be44e97de92ae0d
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-06 04:13:02 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/scipy/find_roots.py
+++ flaskr/tools/numpyPandasPlotly/scipy/find_roots.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import optimize
-
+# compute the root of f(x)=cos(x)−x
+# where x is near −2
 f = lambda x: np.cos(x) - x
 r = optimize.fsolve(f, -2)# look for root in area of x=-2
 print("r =", r) # r = [0.73908513]
