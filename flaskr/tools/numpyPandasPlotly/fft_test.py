4ad46fe3fb2ae581c74b52e9c386ea1c2e11406c
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-07 11:55:18 +0300

    ud

---
+++ flaskr/tools/numpyPandasPlotly/fft_test.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.fft import fft, ifft
+
+sr = 2000
+# sampling interval
+ts = 1.0/sr
+t = np.arange(0,1,ts)
+
+# generate forye serial of 3 freq 1. 4 7 and add them
+freq = 1.
+x = 3*np.sin(2*np.pi*freq*t)
+
+freq = 4
+x += np.sin(2*np.pi*freq*t)
+
+freq = 7
+x += 0.5* np.sin(2*np.pi*freq*t)
+
+X = fft(x)
+N = len(X)
+n = np.arange(N)
+T = N/sr
+freq = n/T
+
+plt.figure(figsize = (12, 6))
+plt.subplot(121)
+
+plt.stem(freq, np.abs(X), 'b', \
+         markerfmt=" ", basefmt="-b")
+plt.xlabel('Freq (Hz)')
+plt.ylabel('FFT Amplitude |X(freq)|')
+plt.xlim(0, 10)
+
+plt.subplot(122)
+plt.plot(t, ifft(X), 'r')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.tight_layout()
+plt.show()
