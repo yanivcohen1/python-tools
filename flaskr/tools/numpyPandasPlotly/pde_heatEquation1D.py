a4fed548f783dfab075778d1c4258624b6be81c8
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-09 08:05:54 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/pde_heatEquation1D.py
+++ flaskr/tools/numpyPandasPlotly/pde_heatEquation1D.py
@@ -4,14 +4,12 @@ import matplotlib.pyplot as plt
 # solve PDE Heat Equation in 1D u(t, x): ∂u/∂t ​= κ*∂²𝑢/∂x² # pylint: disable=E2515
 
 # Defining our problem
-
 k = 110
 length = 50 #mm
 time = 4 #seconds
 nodes = 20
 
 # Initialization
-
 dx = length / nodes
 dt = 0.5 * dx**2 / k
 t_nodes = int(time/dt)
@@ -19,32 +17,24 @@ t_nodes = int(time/dt)
 u = np.zeros(nodes) + 20 # Plate is initially as 20 degres C
 
 # Boundary Conditions
-
-u[0] = 100
-u[-1] = 100
-
+u[0] = 100 # 100 degres C at x = 0
+u[-1] = 100 # 100 degres C at x = L
 
 # Visualizing with a plot
-
 fig, axis = plt.subplots()
-
 pcm = axis.pcolormesh([u], cmap='jet', vmin=0, vmax=100) # cmap=plt.cm.jet
 plt.colorbar(pcm, ax=axis)
 axis.set_ylim([-2, 3])
 
 # Simulating
-
 counter = 0
 
 # u_new is the u(t+dt)
 u_new = u.copy()
 
 while counter < time :
-    # w is the memory for u[i + 1] that will not overide
-
 
     for i in range(1, nodes - 1):
-
         # Discretized Laplacian base on "Central difference Approximating Derivatives"
         # Discretized Laplacian | x=x_i: (u_{i-1} - 2u_i + u_{i+1}) / dx^2
         # see: pde Central difference Approximating.jpg
@@ -60,6 +50,5 @@ while counter < time :
     pcm.set_array([u])
     axis.set_title("Distribution at t: {:.3f} [s].".format(counter))
     plt.pause(0.01)
-
 
 plt.show()
