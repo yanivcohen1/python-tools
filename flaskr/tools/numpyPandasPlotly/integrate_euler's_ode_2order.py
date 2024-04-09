07bcab2348cd8283382fd55b8d2179f269bf42c0
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-09 09:51:03 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/integrate_euler's_ode_2order.py
+++ flaskr/tools/numpyPandasPlotly/integrate_euler's_ode_2order.py
@@ -6,8 +6,12 @@ def f(t, y):
     # y"(t) - t*y'(t) + 3*y(t) = 0
     # y"(t) = t*y'(t) - 3*y(t)
     # y[1] = y' ; y[0] = y
-    return np.array([y[1], t + y[1] - 3 * y[0]]) # [y', y'']
+    # return np.array([y[1], t + y[1] - 3 * y[0]]) # [y', y'']
 
+    # y"(t) + y(t) = 0 , y(0)=2, y'(0)=1
+    return np.array([y[1], -y[0]]) # [y', y'']
+
+# Euler method for solving ODEs y[0]]) # [y', y'']
 def euler(f, tv, y0, N):
     t0, T = tv
     h = (T - t0) / N
@@ -29,9 +33,9 @@ def euler(f, tv, y0, N):
 
 # Initial conditions and parameters
 t0 = 0
-T = 4
-y0 = np.array([1, -2])  # Initial vector [y1, y2]
-N = 256  # Number of steps
+T = 8  # Final time
+y0 = np.array([2, 1])  # Initial vector [y1, y2]
+N = 5000  # Number of steps
 
 # Solve the ODE using Euler method
 ts, ys = euler(f, [t0, T], y0, N)
@@ -40,8 +44,11 @@ yfinal = ys[-1, :]
 # Print the final values
 print(f"y(T) = {yfinal[0]:.5f}, y'(T) = {yfinal[1]:.5f}")
 
+y_exact = np.sin(ts) + 2*np.cos(ts)
+
 # Plot the solution
 plt.plot(ts, ys[:, 0], 'b', label='y(t)') # y' = ys[:, 1]
+plt.plot(ts, y_exact, 'r--', label="The exact solution is y = sin(t) + 2*cos(t)")
 plt.title("Solution y''(t) + t*y'(t) -3*y(t) = 0 with y(0) = 1, y'(0) = -2")
 plt.ylabel('y(t)')
 plt.xlabel('t')
