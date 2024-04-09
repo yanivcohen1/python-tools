8c0551c11c16b106a7805ffd2ef46381e3af5d72
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-09 03:53:38 +0300

    ud

---
+++ flaskr/tools/numpyPandasPlotly/derive_central_difference.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# entral difference method for drive x^2
+
+# Define the function
+def f(x):
+    return x**2
+
+# Define the central difference method
+def central_difference(f, x, h):
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+# Define the range for x and the step size h
+x = np.linspace(-10, 10, 100)
+h = 0.1
+
+# Calculate the derivative using the central difference method
+derivatives = [central_difference(f, xi, h) for xi in x]
+
+# Plot the function and its derivative
+plt.figure(figsize=(10, 5))
+
+# Plot the original function
+plt.subplot(1, 2, 1)
+plt.plot(x, f(x), label='f(x) = x²')
+plt.title('Function x²')
+plt.grid(True)
+plt.legend()
+
+# Plot the derivative
+plt.subplot(1, 2, 2)
+plt.plot(x, derivatives, label="f'(x) Approximation", color='orange')
+plt.title('Derivative is 2x')
+plt.grid(True)
+plt.legend()
+
+plt.tight_layout()
+plt.show()
---
+++ flaskr/tools/numpyPandasPlotly/integrate_euler's_method.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Euler's Method for integrate dy/dx = x + y; y0 = 1
+
+# Define the function that represents the derivative dy/dx
+def derivative(x, y):
+    return x + y
+
+# Initial conditions
+x0 = 0
+y0 = 1
+h = 0.01  # Step size
+x_end = 2
+
+# Create an array of x values from x0 to x_end with a step size h
+x_values = np.arange(x0, x_end + h, h)
+
+# Initialize an array to store the y values
+y_values = [y0]
+
+# Apply Euler's method
+for i in range(1, len(x_values)):
+    x = x_values[i-1]
+    y = y_values[-1]
+    y_new = y + h * derivative(x, y)
+    y_values.append(y_new)
+
+y_exact =  2 * np.exp(x_values) - x_values - 1  # Exact solution
+
+# Plot the differential equation and its solution
+plt.figure(figsize=(12, 5))
+
+# Plot the differential equation
+plt.subplot(1, 2, 1)
+plt.plot(x_values, derivative(x_values, y0), 'r-', label="dy/dx")
+plt.title("Differential Equation dy/dx = x + y; y0 = 1")
+plt.xlabel('x')
+plt.ylabel('dy/dx')
+plt.legend()
+plt.grid(True)
+
+# Plot Euler's method solution
+plt.subplot(1, 2, 2)
+plt.plot(x_values, y_values, 'b-', label="Euler's Method Approximation")
+plt.plot(x_values, y_exact, 'r--', label="The exact solution is y = 2e^x - x - 1")
+plt.title("Euler's Method The exact solution is y = 2e^x - x - 1")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
