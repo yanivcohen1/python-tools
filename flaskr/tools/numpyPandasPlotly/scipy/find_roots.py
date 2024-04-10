3cf6a0d3729311437b93aaa45d2f5a447ef51232
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-10 21:00:24 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/scipy/find_roots.py
+++ flaskr/tools/numpyPandasPlotly/scipy/find_roots.py
@@ -3,9 +3,15 @@ from scipy import optimize
 # compute the root of f(x)=cos(x)−x
 # where x is near −2
 f = lambda x: np.cos(x) - x
-r = optimize.fsolve(f, -2)# look for root in area of x=-2
+r = optimize.fsolve(f, -2, maxfev=1000)# look for root in area of x=-2, in 1000 trays
 print("r =", r) # r = [0.73908513]
-
 # Verify the solution is a root
-result = f(r)
-print("result=", result) # result= [0.]
+print(f"validated root f({r})=", f(r)) # result= [0.]
+
+#---------------------------------------------------------------------
+# compute the root of f(x)=1/x
+f = lambda x: np.sin(x)-0.5
+res = optimize.least_squares(f, (-1,), bounds=(-1, 1)) # bounds ia the area to look roots
+print("r =", res.x)
+print(f"validated root f({res.x})=", f(res.x))
+print(res.message)
