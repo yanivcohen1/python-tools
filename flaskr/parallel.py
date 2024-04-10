058083d9cdf7d4abb33f59781521ec706cbebed5
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-10 12:39:39 +0300

    ud

--- flaskr/parallel.py
+++ flaskr/parallel.py
@@ -2,8 +2,8 @@ import multiprocessing as mp
 import time
 from functools import partial
 
-def random_square_fun(n, a):
-    return n ** 2 + a[0]
+def random_square_fun(n, a, b):
+    return n ** 2 + a[0] + b
 
 if __name__ == "__main__":
     print(f"Number of cpu: {mp.cpu_count()}")
@@ -14,8 +14,7 @@ if __name__ == "__main__":
 
     pool = mp.Pool(processes=n_cpu)
     # results = [pool.map(random_square_fun, range(10000000))]
-    # results = [pool.map(random_square_fun, range(10000000), a_args)]
-    results = [pool.map(partial(random_square_fun, a=a_args), range(10000000))]
+    results = [pool.map(partial(random_square_fun, a=a_args, b=a_args[2]), range(10000000))]
     t1 = time.time()
     print(f'Execution time {t1 - t0} seconds')
     print('First 10 results:', results[0][0:10])
