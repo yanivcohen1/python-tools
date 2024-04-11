d7806066a98038cf8f26228065e3a11871c492f7
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-11 17:00:57 +0300

    ud

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
       "scapy", "psutil", "keyboard", "sortedcontainers", "numpy", "matplotlib", "pandas", "pandas_datareader",
       "plotly", "cufflinks", "ipykernel", "selenium", "webdriver-manager", "sympy", "scipy", "pycryptodome",
       "ipywidgets", "IPython", "seaborn", "nbformat", "openpyxl", "jupyterlab-mathjax3", "google-generativeai",
-      "pint"
+      "pint", "scikit-image"
       ],
     extras_require={"test": ["pytest", "coverage"]},
 )
