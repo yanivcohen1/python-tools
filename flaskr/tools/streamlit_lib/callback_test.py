e84a0c0e77d48fbf773889a86ac6da2625f1bade
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-04-27 04:08:14 +0300

    ud

--- flaskr/tools/streamlit_lib/callback_test.py
+++ flaskr/tools/streamlit_lib/callback_test.py
@@ -1,12 +1,13 @@
 import streamlit as st
-
-st.write("### Callback test")
+import plotly.graph_objs as go
 
 # run once
 if "init" not in st.session_state:
     # Initialize 'options' variable
     st.session_state["init"] = True
     st.session_state["options"] = []
+
+st.write("### Callback test")
 
 # Create a button that populates the 'options' variable when clicked
 if st.button('Load options'):
@@ -26,3 +27,34 @@ if selected_options:
     # Also print each selected option
     for option in selected_options:
         st.write(f"Option: {option}")
+
+
+# plotly test
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(
+    x=[0, 1, 2, 3, 4, 5, 6, 7, 8],
+    y=[0, 1, 2, 3, 4, 5, 6, 7, 8],
+    name="Name of Trace 1"       # this sets its legend entry
+))
+
+
+fig.add_trace(go.Scatter(
+    x=[0, 1, 2, 3, 4, 5, 6, 7, 8],
+    y=[1, 0, 3, 2, 5, 4, 7, 6, 8],
+    name="Name of Trace 2"
+))
+
+fig.update_layout(
+    title="Plot Title",
+    xaxis_title="X Axis Title",
+    yaxis_title="Y Axis Title",
+    legend_title="Legend Title",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="RebeccaPurple"
+    )
+)
+
+st.plotly_chart(fig, use_container_width=True)
