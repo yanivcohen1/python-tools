df5425ad82a6c422ae3f297dd081de6c8fcb7096
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-05-11 19:04:49 +0300

    ud

--- flaskr/tools/vpython_lib/v_test.py
+++ flaskr/tools/vpython_lib/v_test.py
@@ -1,6 +1,5 @@
 # https://www.youtube.com/watch?v=MtG9cueB548
-import vpython
-from vpython import *
+import vpython as vp
 import numpy as np
 import sys
 
@@ -14,25 +13,25 @@ else:
 
 # np.save('3Dpen', np.array([x1,y1,z1,x2,y2,z2]))
 x1, y1, z1, x2, y2, z2 = np.load(py_file)
-ball1 = vpython.sphere(color = color.green, radius = 0.3, make_trail=True, retain=20)
-ball2 = vpython.sphere(color = color.blue, radius = 0.3, make_trail=True, retain=20)
-rod1 = cylinder(pos=vector(0,0,0),axis=vector(0,0,0), radius=0.05) # the stick
-rod2 = cylinder(pos=vector(0,0,0),axis=vector(0,0,0), radius=0.05) # the stick
-base  = box(pos=vector(0,-4.25,0),axis=vector(1,0,0),
-            size=vector(10,0.5,10) ) # the base is the platform
-s1 = cylinder(pos=vector(0,-3.99,0),axis=vector(0,-0.1,0), radius=0.8, color=color.gray(luminance=0.7))# shadow
-s2 = cylinder(pos=vector(0,-3.99,0),axis=vector(0,-0.1,0), radius=0.8, color=color.gray(luminance=0.7))# shadow
+ball1 = vp.sphere(color = vp.color.green, radius = 0.3, make_trail=True, retain=20)
+ball2 = vp.sphere(color = vp.color.blue, radius = 0.3, make_trail=True, retain=20)
+rod1 = vp.cylinder(pos=vp.vector(0,0,0),axis=vp.vector(0,0,0), radius=0.05) # the stick
+rod2 = vp.cylinder(pos=vp.vector(0,0,0),axis=vp.vector(0,0,0), radius=0.05) # the stick
+base  = vp.box(pos=vp.vector(0,-4.25,0),axis=vp.vector(1,0,0),
+            size=vp.vector(10,0.5,10) ) # the base is the platform
+s1 = vp.cylinder(pos=vp.vector(0,-3.99,0),axis=vp.vector(0,-0.1,0), radius=0.8, color=vp.color.gray(luminance=0.7))# shadow
+s2 = vp.cylinder(pos=vp.vector(0,-3.99,0),axis=vp.vector(0,-0.1,0), radius=0.8, color=vp.color.gray(luminance=0.7))# shadow
 
 print('Start')
 i = 0
 while True:
-    rate(30)
+    vp.rate(30)
     i = i + 1
     i = i % len(x1)
-    ball1.pos = vector(x1[i], z1[i], y1[i])
-    ball2.pos = vector(x2[i], z2[i], y2[i])
-    rod1.axis = vector(x1[i], z1[i], y1[i])
-    rod2.pos = vector(x1[i], z1[i], y1[i])
-    rod2.axis = vector(x2[i]-x1[i], z2[i]-z1[i], y2[i]-y1[i])
-    s1.pos = vector(x1[i], -3.99, y1[i])
-    s2.pos = vector(x2[i], -3.99, y2[i])
+    ball1.pos = vp.vector(x1[i], z1[i], y1[i])
+    ball2.pos = vp.vector(x2[i], z2[i], y2[i])
+    rod1.axis = vp.vector(x1[i], z1[i], y1[i])
+    rod2.pos = vp.vector(x1[i], z1[i], y1[i])
+    rod2.axis = vp.vector(x2[i]-x1[i], z2[i]-z1[i], y2[i]-y1[i])
+    s1.pos = vp.vector(x1[i], -3.99, y1[i])
+    s2.pos = vp.vector(x2[i], -3.99, y2[i])
