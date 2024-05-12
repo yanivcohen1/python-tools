f0e0f9fd45e6e9c5d7423c01a779873825b109ec
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-05-12 12:00:57 +0300

    ud

--- flaskr/tools/current_dir.py
+++ flaskr/tools/current_dir.py
@@ -1,5 +1,12 @@
 import os
 
-file_name = "/test.gif"
-full_file_path = str(os.path.dirname(__file__)) + file_name
-print(full_file_path)
+currentDir = os.path.join(os.path.dirname(__file__))
+print('current file name:', os.path.basename(__file__))
+reletive_file_path = "/../dst/target_2.txt"
+full_file_path = str(currentDir) + reletive_file_path
+print('full file path:    ',os.path.normpath(full_file_path))
+
+# for parent dir
+target_path_2 = os.path.join(currentDir, '../dst/target_2.txt')
+print('normalize    : ', os.path.normpath(target_path_2))
+print('file name:    ', os.path.basename(target_path_2))
