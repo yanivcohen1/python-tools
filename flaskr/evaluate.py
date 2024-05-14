d32a69645d442e31182d19db01e43cf38deceb1d
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-05-14 11:21:39 +0300

    ud

--- flaskr/evaluate.py
+++ flaskr/evaluate.py
@@ -7,6 +7,13 @@ def inc2(a):
     global b
     b = a + 2
 
-print(eval('inc1(2)')) # run one line and return a value
+print("eval b = ", eval('inc1(2)')) # run one line and return a value
 exec("inc2(2)") # run multilines and not return val
-print(b)
+print("exec b = ", b)
+
+
+var1 = "yaniv"
+var1_name = f'{var1=}'.split('=')[0] # extract varible name
+print(f'{var1_name} = {var1}')
+exec(f"{var1_name} = 'something else'")
+print(f'exec:\n  {var1_name} = {var1}')
