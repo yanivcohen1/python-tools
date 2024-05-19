53a40e37bf4b918a4a575cf1aaf98d4e4ff8a35c
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-05-19 12:58:23 +0300

    ud

--- flaskr/unpack.py
+++ flaskr/unpack.py
@@ -1,8 +1,16 @@
 def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
+def total2(*args, **kwargs):
+    print("args:", args)
+    print("kwargs:", kwargs)
+
+# tuple
+coins_tupl = 100, 50, 25
+print(total(*coins_tupl), "Knuts")
+
 # *args
-coins_lst = [100, 50, 25]
+coins_lst = [100, 50, 25] # list to tuple
 print(total(*coins_lst), "Knuts")
 
 # **kwargs -key:val
@@ -10,3 +18,5 @@ coins_dict = {"galleons": 100, "sickles": 50, "knuts": 25}
 print(total(**coins_dict), "Knuts")
 
 print("is the same:", total(*coins_lst) == total(**coins_dict))
+
+total2(1,2, a=3, b=4)
