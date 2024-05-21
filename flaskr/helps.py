a1c667d0b0939c6b5cd6e329ebe3d3d58ad86842
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-05-21 15:39:35 +0300

    ud

--- flaskr/helps.py
+++ flaskr/helps.py
@@ -18,3 +18,11 @@ help(os)
 # help(class.funName)
 # obj = class()
 # str(obj)
+
+# ------- fileNmae.py -h or --help
+import argparse
+parser = argparse. ArgumentParser (description="Meow like a cat")
+parser.add_argument("-n", help="number of times to meow")
+args = parser.parse_args()
+for _ in range(int(args.n)):
+    print("meow")
