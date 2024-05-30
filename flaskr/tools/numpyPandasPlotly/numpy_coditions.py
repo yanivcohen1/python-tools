e280e50b174942042692a4a45fcc7fdd86ed2496
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-05-30 19:07:48 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/numpy_coditions.py
+++ flaskr/tools/numpyPandasPlotly/numpy_coditions.py
@@ -9,5 +9,10 @@ print(vals[zero_crossings[0]]) # -1
 # if else inline
 # x=np.random.randint(100, size=(10,5))
 x = np.array([[1,2,3], [3,2,1]])
-out_x = np.where(x<=2, x+1, x-1)
+out_x = np.where(x<=2, x+1, x-1) # like list comprehension
 print(out_x) # [[2 3 2], [2 3 2]]
+
+# filter
+x = np.array([1,2,3,4])
+out_x = x[(x<2) | (x>2)]
+print(out_x) # [1 3 4] no 2
