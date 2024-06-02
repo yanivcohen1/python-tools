1197903d11fca76d37c01504f6d068acd671a22a
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-06-02 13:17:31 +0300

    ud

---
+++ flaskr/tools/numpyPandasPlotly/numpy_polifit.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_d = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+y_d = np.array([0, 0.8, 0.9, 0.1, -0.6, -0.8, -1, -0.9, -0.4])
+
+plt.figure(figsize = (12, 6))
+for i in range(1, 7):
+
+    # get the polynomial coefficients
+    y_est = np.polyfit(x_d, y_d, i)
+    plt.subplot(2,3,i)
+    plt.plot(x_d, y_d, 'o')
+    # evaluate the values for a polynomial
+    plt.plot(x_d, np.polyval(y_est, x_d))
+    plt.title(f'Polynomial order {i}')
+
+plt.tight_layout()
+plt.show()
