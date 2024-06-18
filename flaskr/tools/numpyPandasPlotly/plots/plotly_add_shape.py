9dce0c73bab1bcc2f3a73b535789bc08bc066859
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-06-18 21:15:32 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/plots/plotly_add_shape.py
+++ flaskr/tools/numpyPandasPlotly/plots/plotly_add_shape.py
@@ -20,5 +20,11 @@ fig.add_shape(type="circle",
 fig.add_shape(type="line", xref="x", yref="y",
             x0=3, y0=0.5, x1=5, y1=0.8, line_width=3)
 
+# add Custom shape
+fig.add_shape(type="path",
+              path=" M 2 2 L 2 3 L 4 1 Z",
+              fillcolor="LightPink",
+              line_color="Crimson")
+
 # Show the figure
 fig.show()
