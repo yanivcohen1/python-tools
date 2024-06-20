98852c89a955e61cbebda162ce9452a7623fe069
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-06-20 21:36:18 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/num_py/numpy_manipulation.py
+++ flaskr/tools/numpyPandasPlotly/num_py/numpy_manipulation.py
@@ -4,3 +4,10 @@ a = np.array([1, 2])
 a1 = np.vstack((a, [3, 4])) # [[1, 2], [3, 4]]
 x, y = a1.T # x =[1,3], y=[2,4]
 print('x =', x, ', y =', y) # x =[1,3], y=[2,4]
+
+
+# equvalent in python native
+a = [[1, 2]]
+a.append([3, 4]) # [[1, 2], [3, 4]]
+x, y = zip(*a) # x =[1,3], y=[2,4]
+print('x =', list(x), ', y =', y) # x =[1,3], y=[2,4]
