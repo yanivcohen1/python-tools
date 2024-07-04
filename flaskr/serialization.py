960d86638b628241af4405362270a364bd283934
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-04 15:14:21 +0300

    ud

---
+++ flaskr/serialization.py
@@ -0,0 +1,25 @@
+import pickle
+
+class MyClass2:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def reads(self):
+        print("filename:", self.filename)
+class MyClass:
+    """Print and number lines in a text file."""
+    def __init__(self, filename, num, obj):
+        self.filename = filename
+        self.lineno = num
+        self.obj = obj
+
+    def reads(self):
+        print("filename:", self.filename, ", lineno:", self.lineno,
+                ", class2:", self.obj.filename)
+
+reader1 = MyClass2("my file_2")
+reader2 = MyClass("my file_1", 22, MyClass2("my file_3"))
+
+new_reader  = pickle.loads(pickle.dumps([reader1, reader2]))
+for reader in new_reader:
+    reader.reads()
