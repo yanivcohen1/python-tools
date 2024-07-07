68ba35405e0ffe0d44a8cde8438edb3e6dc5b189
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-07 14:59:13 +0300

    ud

---
+++ flaskr/tools/numpyPandasPlotly/plots/plot_histegram.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create a random number generator with a fixed seed for reproducibility
+rng = np.random.default_rng(19680801)
+
+N_points = 100000
+n_bins = 20
+
+# Generate two normal distributions
+dist1 = rng.standard_normal(N_points)
+dist2 = 0.4 * rng.standard_normal(N_points) + 5
+
+fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+
+# We can set the number of bins with the *bins* keyword argument.
+axs[0].hist(dist1, bins=n_bins)
+axs[1].hist(dist2, bins=n_bins)
+
+axs[0].set_xlabel("values")
+axs[0].set_ylabel("how many")
+axs[1].set_xlabel("values")
+
+plt.show()
