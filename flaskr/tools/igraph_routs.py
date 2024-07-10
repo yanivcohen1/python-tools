fa7cbcceb357a6688d2687c608364c65f68961a5
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-10 19:58:12 +0300

    ud

--- flaskr/tools/igraph_routs.py
+++ flaskr/tools/igraph_routs.py
@@ -1,24 +1,25 @@
 import igraph as ig
 
-g = ig.Graph(directed=True)
-g.add_vertices(6)
-g.vs["label"] = ["A", "B", "C", "D", "E", "F"]  # Assign labels to nodes
-routs = [("A", "B"), ("A", "C"), ("B", "D"), ("C", "D"), ("C", "E"), ("D", "F"), ("E", "F")]
-for rout in routs:
-    g.add_edges([tuple([g.vs.find(label=name).index for name in rout])])
-
-g.es["weight"] = [2, 1, 5, 4, 7, 3, 2]  # Assign weights to edges
 # Define your source and destination nodes
 source = "A"
 destination = "F"
 
+g = ig.Graph(directed=True)
+g.add_vertices(6)
+g.vs["label"] = ["A", "B", "C", "D", "E", "F"]  # Assign labels to nodes
+routs = [(2, "A", "B"), (1, "A", "C"), (5, "B", "D"), (4, "C", "D"), (7, "C", "E"), (3, "D", "F"), (2, "E", "F")]
+weights = []
+for rout in routs:
+    g.add_edges([tuple([g.vs.find(label=name).index for name in rout[1:]])])
+    weights.append(rout[0])
+
+g.es['weight'] = weights # Assign weights to edges
 # Find all simple paths between source and destination
 all_paths = g.get_all_simple_paths(g.vs.find(label=source).index, g.vs.find(label=destination).index)
-
 resoults = []
 print("All possible routes from source to destination:")
 for path in all_paths:
-    total_weight = sum(g.es[e]["weight"] for e in path)
+    total_weight = sum(g.es[e]['weight'] for e in path)
     names = [g.vs[e]["label"] for e in path]
     resoults.append({
                     "path": names,
