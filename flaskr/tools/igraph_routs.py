3131448a9d5868037dd77234b2182af1810101f6
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-12 20:41:28 +0300

    ud

--- flaskr/tools/igraph_routs.py
+++ flaskr/tools/igraph_routs.py
@@ -1,24 +1,40 @@
 import igraph as ig
+import pandas as pd
+import matplotlib.pyplot as plt
+
+g = ig.Graph(directed=True)
+g.add_vertices(6)
+g.vs["label"] = ["A", "B", "C", "D", "E", "F"]
 
 # Define your source and destination nodes
 source = "A"
 destination = "D"
 
-g = ig.Graph(directed=True)
-g.add_vertices(6)
-g.vs["label"] = ["A", "B", "C", "D", "E", "F"]  # Assign labels to nodes
-g.es['weight'] = []
-# build Graph from panda data_frame
-# G_airtraffic = ig.Graph.DataFrame(data_frame.loc[:, ['source', 'dest']], use_vids=False)
-routs = [(2, "A", "B"), (1, "A", "C"), (5, "B", "D"), (4, "C", "D"), (7, "C", "E"), (3, "D", "F"), (2, "E", "F")]
-for rout in routs:
-    g.add_edges([[g.vs.find(label=name).index for name in rout[1:]]])
-    g.es['weight'] = g.es['weight'][:-1] + [rout[0]]
+df = pd.DataFrame({
+    'source': ['A', 'A', 'A', 'A', 'C'],
+    'target': ['B', 'C', 'D', 'E', 'D'],
+    'weight': [ 1 ,  2 ,  3 ,  4 ,  5 ]
+})
+
+# Generate the graph
+g = g.TupleList(df.itertuples(index=False),
+                        directed=True,
+                        edge_attrs='weight',
+                        vertex_name_attr='label')  # Remove 'weights=True'
+
+# Choose a layout algorithm (e.g., Kamada-Kawai)
+layout = g.layout("kamada_kawai")
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+# Plot the graph with edge weights displayed
+ig.plot(g, layout=layout, target=ax, edge_label=df['weight'].tolist())
+ax.set_title(f"igraph auto from panda")
+plt.show()
 
 # Find all simple paths between source and destination
 all_paths = g.get_all_simple_paths(g.vs.find(label=source).index, g.vs.find(label=destination).index)
 resoults = []
-print("All possible routes from source to destination:")
+print("All possible routes from source to destination sort by weights:")
 for path in all_paths:
     total_weight = 0
     for i in range(len(path)-1):
