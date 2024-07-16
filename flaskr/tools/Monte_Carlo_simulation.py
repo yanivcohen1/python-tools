a7cce1f3c9200388ea5be72fb82552b65ea9e6d4
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-16 11:20:23 +0300

    ud

--- flaskr/tools/Monte_Carlo_simulation.py
+++ flaskr/tools/Monte_Carlo_simulation.py
@@ -2,16 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-num_sims = 1000000 # number of trays
+num_sims = 1_000_000 # number of trays
 
-A = np.random.uniform(1, 5, num_sims) # uniform prabbility between 1 to 5
-B = np.random.uniform(2, 6, num_sims) # uniform prabbility between 2 to 6
+A = np.random.uniform(1, 5, num_sims) # uniform prabbility between 1 to 5 hours
+B = np.random.uniform(2, 6, num_sims) # uniform prabbility between 2 to 6 hours
 
-duration = A + B # probbility of a and b for finish A and then finish B
+duration = A + B # probbility of a and b (finish A and then finish B)
+finish_time = 9 # in hours
 
 plt.figure()
 plt.hist(duration, density = True, edgecolor='white')
-plt.axvline(9, color='r')
-res = (duration > 9).sum()/num_sims * 100 # 100 for precentages
-plt.title(f"your probbility of a and b after 9 is: {res:.2f}%")
+plt.axvline(finish_time, color='r')
+# sum of bools ((duration > finish_time).sum())
+res = (duration > finish_time).sum()/num_sims * 100 # precentages for above finish_time
+plt.title(f"your probbility of a and b done above {finish_time} hours is: {res:.2f}%")
 plt.show()
