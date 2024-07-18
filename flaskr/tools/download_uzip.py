0c2cce28a62de0089e25885bf2787dc83e856169
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-18 14:39:23 +0300

    ud

---
+++ flaskr/tools/download_uzip.py
@@ -0,0 +1,10 @@
+import wget
+filename = wget.download('https://github.com/AlexMa123/BIU_Computational_physics_2023/raw/main/data.zip')
+
+import zipfile
+with zipfile.ZipFile(filename, 'r') as zip_ref:
+    # Extract the files to a specified directory (e.g., 'extracted_files')
+    zip_ref.extractall()
+
+import os
+os.remove(filename)
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
       "plotly", "cufflinks", "ipykernel", "selenium", "webdriver-manager", "sympy==1.6.2", "scipy", "pycryptodome",
       "ipywidgets", "IPython", "seaborn", "nbformat", "openpyxl", "jupyterlab-mathjax3", "google-generativeai",
       "pint", "scikit-image", "streamlit", "vpython", "pygame", "numba", "pymunk", "opencv-python", "pyautogui",
-      "yfinance", "openai", "igraph"
+      "yfinance", "openai", "igraph", "wget"
       ], # "cupy"
     extras_require={"test": ["pytest", "coverage"]},
 )
