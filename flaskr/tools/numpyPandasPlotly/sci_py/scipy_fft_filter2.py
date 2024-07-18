83bc2405f03483ac86b3a370bd6ac6ec3ad8e8f3
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-18 12:24:42 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/sci_py/scipy_fft_filter2.py
+++ flaskr/tools/numpyPandasPlotly/sci_py/scipy_fft_filter2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter, freqz
 
 # Generate a 1 kHz sine wave with noise
 fs = 10_000  # Sampling frequency (Hz)
@@ -7,21 +8,22 @@ t = np.linspace(0, 1, fs, endpoint=False)
 signal = np.sin(2 * np.pi * 1000 * t) + np.random.normal(0, 0.5, len(t))
 
 # Compute the FFT of the signal
-fft_result = np.fft.fft(signal)
-freq = np.fft.fftfreq(len(t), d=1/fs)
+fft_result = np.fft.rfft(signal)
+freq = np.fft.rfftfreq(len(t), d=1/fs)
+
+max_signal_freq = freq[np.argmax(np.abs(fft_result))]
 
 # Design a bandpass filter
-filter_wide = 10
-max_signal_freq = np.abs(freq[np.argmax(np.abs(fft_result))])
-lowcut, highcut = max_signal_freq-filter_wide, max_signal_freq+filter_wide  # Frequency range (Hz)
-sig_rfft_filtered = fft_result.copy()
-sig_rfft_filtered[(freq < lowcut) | (freq > highcut)] = 0
-sig_lfft_filtered = fft_result.copy()
-sig_lfft_filtered[(-freq < lowcut) | (-freq > highcut)] = 0
-sig_fft_filtered = sig_rfft_filtered + sig_lfft_filtered
-# get the filtered signal in time domain
-filtered_signal = np.fft.ifft(sig_fft_filtered)
+band_wide = 10
+lowcut, highcut = max_signal_freq-band_wide, max_signal_freq+band_wide  # Frequency range (Hz)
+order = 4  # Filter order
+nyq = 0.5 * fs
+low = lowcut / nyq
+high = highcut / nyq
+b, a = butter(order, [low, high], btype='band')
 
+# Apply the filter
+filtered_signal = lfilter(b, a, signal)
 
 # Plot the original signal, FFT, and filtered signal
 plt.figure(figsize=(10, 6))
@@ -33,26 +35,27 @@ plt.xlim(0.10, 0.20)
 plt.legend()
 
 plt.subplot(4, 1, 2)
-plt.plot(freq, np.abs(fft_result), label='FFT original')
+plt.plot(freq, np.abs(fft_result), label='rFFT')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Amplitude')
 plt.ylim(0, 300)
-plt.xlim(0, 2000)
 plt.legend()
 
 plt.subplot(4, 1, 3)
-plt.plot(t, filtered_signal, label='Filtered Signal') # mulltiply 2 for bout sides
+plt.plot(t, filtered_signal, label='Filtered Signal')
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.xlim(0.10, 0.20)
 plt.legend()
 
+# Compute the FFT of the signal
+fft_result2 = np.fft.fft(filtered_signal)
+
 plt.subplot(4, 1, 4)
-plt.plot(freq, np.abs(sig_fft_filtered), label=f'FFT filtered wide {filter_wide} hz') # mulltiply 2 for bout sides
+plt.plot(freq, np.abs(fft_result2[0:len(freq)]), label='FFT filtered')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Amplitude')
 plt.ylim(0, 300)
-plt.xlim(0, 2000)
 plt.legend()
 
 plt.tight_layout()
