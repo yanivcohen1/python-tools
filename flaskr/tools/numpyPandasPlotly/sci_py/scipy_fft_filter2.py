34dd13d94a3b0955c091c9e813dfeea7110df243
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-18 12:11:46 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/sci_py/scipy_fft_filter2.py
+++ flaskr/tools/numpyPandasPlotly/sci_py/scipy_fft_filter2.py
@@ -9,17 +9,20 @@ t = np.linspace(0, 1, fs, endpoint=False)
 signal = np.sin(2 * np.pi * 1000 * t) + np.random.normal(0, 0.5, len(t))
 
 # Compute the FFT of the signal
-fft_result = np.fft.rfft(signal)
-freq = np.fft.rfftfreq(len(t), d=1/fs)
+fft_result = np.fft.fft(signal)
+freq = np.fft.fftfreq(len(t), d=1/fs)
 
 # Design a bandpass filter
-max_signal_freq = freq[np.argmax(np.abs(fft_result))]
-lowcut, highcut = max_signal_freq-10, max_signal_freq+10  # Frequency range (Hz)
-sig_fft_filtered = fft_result.copy()
-sig_fft_filtered[(freq < lowcut) | (freq > highcut)] = 0
-
+filter_wide = 10
+max_signal_freq = np.abs(freq[np.argmax(np.abs(fft_result))])
+lowcut, highcut = max_signal_freq-filter_wide, max_signal_freq+filter_wide  # Frequency range (Hz)
+sig_rfft_filtered = fft_result.copy()
+sig_rfft_filtered[(freq < lowcut) | (freq > highcut)] = 0
+sig_lfft_filtered = fft_result.copy()
+sig_lfft_filtered[(-freq < lowcut) | (-freq > highcut)] = 0
+sig_fft_filtered = sig_rfft_filtered + sig_lfft_filtered
 # get the filtered signal in time domain
-filtered_signal = np.fft.irfft(np.abs(sig_fft_filtered))
+filtered_signal = np.fft.ifft(sig_fft_filtered)
 
 
 # Plot the original signal, FFT, and filtered signal
@@ -32,7 +35,7 @@ plt.xlim(0.10, 0.20)
 plt.legend()
 
 plt.subplot(4, 1, 2)
-plt.plot(freq, np.abs(fft_result), label='rFFT')
+plt.plot(freq, np.abs(fft_result), label='FFT')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Amplitude')
 plt.ylim(0, 300)
@@ -40,14 +43,14 @@ plt.xlim(0, 2000)
 plt.legend()
 
 plt.subplot(4, 1, 3)
-plt.plot(t, filtered_signal, label='Filtered Signal')
+plt.plot(t, filtered_signal, label='Filtered Signal') # mulltiply 2 for bout sides
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.xlim(0.10, 0.20)
 plt.legend()
 
 plt.subplot(4, 1, 4)
-plt.plot(freq, sig_fft_filtered, label='rFFT filtered')
+plt.plot(freq, np.abs(sig_fft_filtered), label='FFT filtered') # mulltiply 2 for bout sides
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Amplitude')
 plt.ylim(0, 300)
