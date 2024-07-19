cec80a4a7ae39ab0822aff99777eec57c87d6664
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-07-19 16:49:21 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/sci_py/2d_image_fft.py
+++ flaskr/tools/numpyPandasPlotly/sci_py/2d_image_fft.py
@@ -7,26 +7,42 @@ import numpy as np
 img_load = io.imread('flaskr/tools/numpyPandasPlotly/sci_py/2d_image_flower.PNG')
 rgb_img = color.rgba2rgb(img_load)
 gray_img = color.rgb2gray(rgb_img)
-plt.imshow(gray_img, cmap='gray')
-plt.show()
+# plt.imshow(gray_img, cmap='gray')
+# plt.show()
+
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(7, 6))
+
+ax[0,0].imshow(gray_img, cmap='gray')
+ax[0,0].set_title(f'original image')
 
 # fft in 2D
-img_FT = fft2(gray_img)
+fft_image = fft2(gray_img)
 fy = np.fft.fftfreq(gray_img.shape[0],d=10) #suppose the spacing between pixels is 10mm, for example
 fx = np.fft.fftfreq(gray_img.shape[1],d=10)
-print('{:.2f} correponds to fx={:.6f} and fy={:.6f}'.format(img_FT[10,20], fx[20], fy[10]))
-plt.imshow(np.abs(img_FT), cmap='gray', vmax=50)
-plt.colorbar()
-plt.show()
+print('{:.2f} correponds to fx={:.6f} and fy={:.6f}'.format(fft_image[10,20], fx[20], fy[10]))
+
+ax[0,1].imshow(np.log1p(np.abs(fft_image)), cmap='gray')
+ax[0,1].set_title('fft image')
+# plt.imshow(np.abs(img_FT), cmap='gray', vmax=50)
+# plt.colorbar()
+# plt.show()
 
 # Remove low frequencies
-img_FT_alt = np.copy(img_FT)
+img_FT_alt = np.copy(fft_image)
 img_FT_alt[-2:] = 0
 img_FT_alt[:,-2:] = 0
 img_FT_alt[:2] = 0
 img_FT_alt[:,:2] = 0
 # inverse fft in 2D
+
+ax[1,1].imshow(np.log1p(np.abs(img_FT_alt)), cmap='gray')
+ax[1,1].set_title('fft filterd image')
+
 img_alt = np.abs(ifft2(img_FT_alt))
-plt.imshow(img_alt, cmap='gray')
-plt.colorbar()
+ax[1,0].imshow(img_alt, cmap='gray')
+ax[1,0].set_title(f'filterd image')
+
+# plt.imshow(img_alt, cmap='gray')
+# plt.colorbar()
+plt.tight_layout
 plt.show()
