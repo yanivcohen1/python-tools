ca964cb698c78936f37eb85199a0890351df2ecf
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-08-06 02:31:51 +0300

    ud

---
+++ flaskr/tools/3blue1brown.py
@@ -0,0 +1,26 @@
+# https://docs.manim.community/en/stable/examples.html
+# winget install ffmpeg
+# manim filemane.py className -pql
+# manim -pql 3blue1brown.py Main
+# vlc open first
+# run from debug select "Python:Manim"
+
+# https://github.com/3b1b/manim
+# https://www.youtube.com/watch?v=d9nbtyO2YcU
+from manim import *
+
+class Main(Scene):
+    def construct(self):
+        circle = Circle(radius=1, color=BLUE)
+        dot = Dot()
+        dot2 = dot.copy().shift(RIGHT)
+        self.add(dot)
+
+        line = Line([3, 0, 0], [5, 0, 0])
+        self.add(line)
+
+        self.play(GrowFromCenter(circle))
+        self.play(Transform(dot, dot2))
+        self.play(MoveAlongPath(dot, circle), run_time=2, rate_func=linear)
+        self.play(Rotating(dot, about_point=[2, 0, 0]), run_time=1.5)
+        self.wait()
