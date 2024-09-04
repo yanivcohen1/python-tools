c691dfc33eb7c55040183e4d9ff4607a11db4a93
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-09-04 07:33:08 +0300

    ud

--- flaskr/runCmdTest.py
+++ flaskr/runCmdTest.py
@@ -2,7 +2,7 @@ import subprocess
 import platform
 import os
 # result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE) # linux shell
-# result = subprocess.run(['cmd', '/c', 'dir'], stdout=subprocess.PIPE) # windows cmd
+# result = subprocess.run(['cmd', '/c', 'dir', 'C:\\Temp'], stdout=subprocess.PIPE) # windows cmd
 # result = subprocess.run(['powershell', '/c', 'cat', 'setup.cfg'], stdout=subprocess.PIPE) # windows powershell
 
 # for multiplatform run cmd.sh that contain #!/bin/bash ls -s"
