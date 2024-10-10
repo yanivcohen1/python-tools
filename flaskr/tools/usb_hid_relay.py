be1cb015e6a892c679f8c15a45a8980ed06cf8ff
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-10-10 18:34:18 +0300

    ud

---
+++ flaskr/tools/usb_hid_relay.py
@@ -0,0 +1,33 @@
+# https://github.com/JPEWdev/pyhid-usb-relay
+# hid - human interface device
+# #! /usr/bin/env python3
+import time
+import pyhid_usb_relay
+
+if __name__ == "__main__":
+    relay = pyhid_usb_relay.find()
+    print(relay.serial)
+    # Example of reading state and toggling relay #1
+    print("relay_1 is", relay.get_state(1))
+    if relay.get_state(1):
+        relay.toggle_state(1)
+        time.sleep(3)
+        print("relay_1 after toggle is", relay.get_state(1))
+
+    if not relay.get_state(1):
+        relay.toggle_state(1)
+        time.sleep(3)
+        print("relay_1 after toggle is", relay.get_state(1))
+
+    # # You can also refer to relays by index
+    # if relay[1]:
+    #     relay[1] = False
+    # time.sleep(3)
+
+    # if not relay[1]:
+    #     relay[1] = True
+    # time.sleep(3)
+
+    # # If you have relay aliases defined in your config file, you can also refer
+    # # to them in place of the index:
+    # relay["relay1"] = not relay["relay1"] # raspberrypi4
