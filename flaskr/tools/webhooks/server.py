6c61ce97a549a599869c5867db1a1f2377780244
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-10-20 12:07:50 +0300

    ud

---
+++ flaskr/tools/webhooks/server.py
@@ -0,0 +1,20 @@
+import time
+from flask import Flask, Response, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+def event_stream():
+    while True:
+        time.sleep(1)
+        yield 'data: The time is now {}\n\n'.format(time.ctime())
+
+@app.route('/events')
+def stream():
+    return Response(event_stream(), mimetype="text/event-stream")
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
