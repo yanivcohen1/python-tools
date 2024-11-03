f2d561348ceb949f54a30b7bb63482d36ecaa2a2
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-11-03 17:44:04 +0200

    ud

--- flaskr/tools/numpyPandasPlotly/sci_py/lambertw_fun.py
+++ flaskr/tools/numpyPandasPlotly/sci_py/lambertw_fun.py
@@ -4,3 +4,11 @@ import numpy as np
 result = lambertw(4)
 print(result)
 print("~1.2 ==",np.real(result))
+
+# x = lambertw(x*e^x)
+# x^x=4
+# x = e^W(ln(4)) == 2
+# x = ln(4)/W(ln(4))
+
+# 0.5^x = x
+# x = W(ln(2))/ln(2) =~ 1.2
