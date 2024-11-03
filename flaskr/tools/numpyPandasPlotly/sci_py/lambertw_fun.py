bd7eca6c3e4243d62e0377c17503b19e77d0caad
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-11-03 18:32:35 +0200

    ud

--- flaskr/tools/numpyPandasPlotly/sci_py/lambertw_fun.py
+++ flaskr/tools/numpyPandasPlotly/sci_py/lambertw_fun.py
@@ -3,12 +3,22 @@ import numpy as np
 
 result = lambertw(4)
 print(result)
-print("~1.2 ==",np.real(result))
 
 # x = lambertw(x*e^x)
 # x^x=4
-# x = e^W(ln(4)) == 2
-# x = ln(4)/W(ln(4))
+# x*ln(x) = ln(4) , y = ln(x) => x=e^y
+# y*e^y = ln(4) => W(y) = W(ln(4))
+# y = ln(x) = W(ln(4))
+# x = e^W(ln(4)) => x = 2
+print("x^x = 4")
+print("x = e^W(ln(4)) => x =",np.real(np.exp(lambertw(np.log(4)))))
 
 # 0.5^x = x
-# x = W(ln(2))/ln(2) =~ 1.2
+# 0.5 = x ^ 1/x
+# ln(0.5) = 1/x * ln(x), y = ln(x) => 1/x = 1/e^y = e^-y
+# -y*e^-y = -ln(0.5) => W(-y) = W(-ln(0.5))
+# W(-ln(x)) = W(-ln(0.5))
+# -ln(x) = W(-ln(0.5))
+# x = e^-W(-ln(0.5)) =~ 0.641
+print("0.5^x = x")
+print("x = e^-W(ln(0.5)) => x =",np.real(np.exp(-lambertw(-np.log(0.5)))))
