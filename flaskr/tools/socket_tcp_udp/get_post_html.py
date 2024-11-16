1d369e2fbdcb848b93a3f72901ca43eef267e812
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-11-16 09:12:51 +0200

    ud

--- flaskr/tools/socket_tcp_udp/get_post_html.py
+++ flaskr/tools/socket_tcp_udp/get_post_html.py
@@ -21,6 +21,19 @@ def post_json(URL_WithParams, json, headers):
     # print('post:', resp.text[:7])
     return resp.json()# return as json
 
+def send_rcv_cookies_and_download_file():
+    session = requests.Session()
+    url = 'http://google.com/favicon.ico'
+    cookies = {'cookies_are': 'working'}
+    myJson = {'somekey': 'somevalue'}
+    headers={"Content-Type":"json"}
+    response = session.post(url, allow_redirects=True, headers=headers, cookies=cookies, json = myJson)
+    open('google.ico', 'wb').write(response.content) # download file
+    print("print the cookies", session.cookies.get_dict())
+    print("print the headers", response.headers)
+    print("json of response", response.json)
+    print("text of response", response.text)
+
 def get_socket():
     # get or post with socket
     # get http://ip-api.com/json/24.48.0.1
