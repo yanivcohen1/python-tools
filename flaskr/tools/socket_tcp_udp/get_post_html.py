428dec1f3f60c54e6deab7d1fb002b94554df400
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-11-18 18:09:09 +0200

    ud

--- flaskr/tools/socket_tcp_udp/get_post_html.py
+++ flaskr/tools/socket_tcp_udp/get_post_html.py
@@ -30,6 +30,8 @@ def send_rcv_cookies_and_download_file():
     response = session.post(url, allow_redirects=True, headers=headers, cookies=cookies, json = myJson)
     open('google.ico', 'wb').write(response.content) # download file
     session.cookies.set("cookie_name", "cookie_value")
+    session.cookies.set("cookie_name2", "cookie_value2")
+    session.cookies.get("cookie_name")
     print("print the cookies", session.cookies.get_dict())
     print("print the headers", response.headers)
     print("json of response", response.json)
