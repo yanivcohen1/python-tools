3de5611875dee6c4cb371bb7e01221e0f3af639e
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-11-27 10:22:50 +0200

    add yaml test

---
+++ flaskr/tools/yaml_test.py
@@ -0,0 +1,8 @@
+import os
+import yaml
+
+currentDir = os.path.join(os.path.dirname(__file__))
+with open(currentDir + '\config_dev.yml', 'r') as file:
+    prime_service = yaml.safe_load(file)
+
+print(prime_service['rest']['url'])
