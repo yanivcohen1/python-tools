0fbdf0322214e0f6e5a5db2a917a298edfd9a5a2
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-12-03 20:36:26 +0200

    ud

--- flaskr/tools/scapy/scapy_http_sniff.py
+++ flaskr/tools/scapy/scapy_http_sniff.py
@@ -1,8 +1,13 @@
 import json
+from collections import defaultdict
 from scapy.all import *
 from scapy.layers.http import HTTPRequest, HTTPResponse
 from scapy.layers.inet import UDP, TCP, IP, Ether
+from scapy.all import *
+from scapy.layers.http import HTTPRequest, HTTPResponse
 
+# Dictionary to store requests by the transaction ID
+requests = defaultdict(dict)
 # Define the IP address and port to filter
 target_ip = "54.187.16.171"
 target_port = 80
@@ -10,9 +15,16 @@ target_port = 80
 def process_packet(packet):
     if packet.haslayer(HTTPRequest) and packet[IP].dst == target_ip:
         # Modify the HTTP request
-        print(packet)
+        print("req:",packet)
         if packet.haslayer(Raw) and b'application/json' in packet[HTTPRequest].Content_Type:
             print("payload:", packet[Raw].load)
+        # Extract request information
+        http_request = packet[HTTPRequest]
+        transaction_id = (packet[IP].src, packet[TCP].dport, packet[TCP].seq)
+        url = f"http://{http_request.Host.decode()}{http_request.Path.decode()}"
+
+        # Store URL with the transaction ID
+        requests[transaction_id]['url'] = url
         if packet[HTTPRequest].Path == b"/user/" and packet.haslayer(Raw):
             payload_bin = packet[Raw].load
             payload_dict = json.loads(payload_bin)
@@ -28,9 +40,16 @@ def process_packet(packet):
         # sendp(packet) # , iface="eth0"
 
     if packet.haslayer(HTTPResponse) and packet[IP].src == target_ip:
+        transaction_id = (packet[IP].dst, packet[TCP].sport, packet[TCP].ack - 1)
+        if transaction_id in requests:
+            # Retrieve and print the URL
+            url = requests[transaction_id].get('url')
+            if url:
+                print(f"res_URL: {url}")
         if packet.haslayer(Raw) and b'application/json' in packet[HTTPResponse].Content_Type:
             if packet[Raw].load != b'[]':
-                print("response:",packet[Raw].load)
+                print("res:",packet)
+                print("res:",packet[Raw].load)
 
 # Sniff HTTP packets from the specified IP and port
 sniff(filter=f"tcp and host {target_ip} and port {target_port}", prn=process_packet, store=False)
