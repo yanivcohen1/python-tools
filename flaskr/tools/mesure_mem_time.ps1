e518ede719d368cbb53fe21fdb7001273a672035
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-12-09 11:04:26 +0200

    ud

--- flaskr/tools/mesure_mem_time.ps1
+++ flaskr/tools/mesure_mem_time.ps1
@@ -1,5 +1,12 @@
 # Measure command execution time and memory usage
 #.\mesure_mem_time.ps1 -cmd "cmd to run" -cmd2 "cmd2 to run"
+
+# for linux
+# /usr/bin/time -v "command to run"
+# from the output
+# User time (seconds): 0.33
+# Maximum resident set size (kbytes): 3192
+
 param ( [string]$cmd, [string]$cmd2 )
 $process = Start-Process $cmd -PassThru
 Start-Sleep -Milliseconds 100
---
+++ flaskr/tools/mesure_mem_time.sh
@@ -0,0 +1,4 @@
+/usr/bin/time -v "command to run"
+# from the output
+# User time (seconds): 0.33
+# Maximum resident set size (kbytes): 3192
