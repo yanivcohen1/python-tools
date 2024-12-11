230cb8d8b84d9c946f1e314d506d982bf7b56023
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-12-11 14:12:39 +0200

    ud

---
+++ flaskr/tools/py_test/sample.py
@@ -0,0 +1,21 @@
+import random
+import time
+import requests
+
+def random_sum():
+    a = random.randint(1, 10)
+    b = random.randint(1,7)
+    return a + b
+
+def silly():
+    params = {
+    "timestamp": time.time(),
+    "number": random.randint(1, 6)
+    }
+    response = requests.get("https://httpbin.org/get", params)
+    if response.status_code == 200:
+        return response.json()['args']
+
+if __name__ == '__main__':
+    print(random_sum()) # 5
+    print(silly()) # {'number': '6', 'timestamp': '1733916375.8652258'}
