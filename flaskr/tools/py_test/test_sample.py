252d2b7c9cdd3e27c1710378bc73a5a740de903c
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-12-11 16:16:24 +0200

    ud

--- flaskr/tools/py_test/test_sample.py
+++ flaskr/tools/py_test/test_sample.py
@@ -1,15 +1,29 @@
 
 # all tested need to be in format test_*.py
 # in file setup.cfg in section [tool:pytest] testpaths=flaskr/tools/py_test
+# in settings.json
+""" "python.testing.unittestArgs": [
+        "-v",
+        "-s",
+        ".flaskr/tools/py_test",
+        "-p",
+        "test_*.py"
+    ],
+    "python.testing.pytestEnabled": true,
+    "python.testing.unittestEnabled": false,
+    "python.testing.pytestArgs": [
+      "flaskr/tools/py_test"
+    ] """
 # run the all tests ">pytest"
 # run the spcific tests ">pytest -q test_class.py"
+# from testing tab select tests to run
 from unittest import mock
 from unittest.mock import call
 from flaskr.tools.py_test.sample import random_sum, silly
 
 @mock.patch("flaskr.tools.py_test.sample.random.randint")
 def test_random_sum(mock_randint):
-    mock_randint.side_effect = [3, 4]
+    mock_randint.side_effect = [3, 4] # first run return 3 second run return 4
     assert random_sum() == 7
     mock_randint.assert_has_calls(calls=[call(1, 10), call(1, 7)])
 
