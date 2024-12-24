2a4e94d25a4b444c2cbc8bc83d62e7a39ad7ac29
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2024-12-24 11:38:04 +0200

    ud

---
+++ flaskr/tools/day_time.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+
+# Get the current date and time
+now = datetime.now()
+
+# Add one day and one minute
+new_time = now + timedelta(days=1, minutes=1)
+
+# Print the new date and time
+print("Current date and time:", now)
+print("New date and time:", new_time)
+print("format Current date and time:", now.strftime("%d/%m/%Y %H:%M:%S"))
+print(f'{now.day:2.0f}/{now.month:2.0f} {now.hour:2.0f}:{now.minute:2.0f}')
