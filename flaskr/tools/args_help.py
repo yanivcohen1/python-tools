1d0d0170a6299f2ec0362964945b82603faf0c28
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-01-12 19:34:03 +0200

    ud

---
+++ flaskr/tools/args_help.py
@@ -0,0 +1,15 @@
+import argparse
+
+file_path = r"C:\pyProj\adi\third_year\proj3\files\tmp\login_data.gzip.aes"
+key_path = r".\adi\third_year\proj3\keys.key"
+decrypt = True
+parser = argparse.ArgumentParser(description="A simple command line tool that encrypts and decrypts files using AES encryption and stores the key in a separate file.")
+parser.add_argument('-f', '--file_path', type=str, default=file_path, help="File path to encrypt or decyrpt")
+parser.add_argument('-k', '--key_path', type=str, default=key_path, help="File path to save the key or read the key")
+parser.add_argument('-d', '--decrypt', default=decrypt, help="Option that switchs to decyrpt mode, default is encyrpt")
+
+args = parser.parse_args()
+# Access the argument
+print(f'file_path: {args.file_path}')
+print(f'key_path: {args.key_path}')
+print(f'decrypt: {args.decrypt}')
