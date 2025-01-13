e88c9cbdb2895be79e1643983b9e1f60844dc092
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-01-13 19:49:48 +0200

    ud

--- flaskr/asyncio_test2.py
+++ flaskr/asyncio_test2.py
@@ -10,10 +10,11 @@ async def unblock_function():
 
 async def main():
     print("Starting blocking function")
-    start = time.time()
+    start1 = time.time()
     end1 = await unblock_function()
-    end2 = await asyncio.gather(unblock_function(), unblock_function())
-    print(f'run 1 fun took {end1-start:.2f} seconds')
-    print(f'run 2 funs took {max(end2)-end1:.2f} seconds')
+    results = await asyncio.gather(unblock_function(), unblock_function())
+    end2 = time.time()
+    print(f'run 1 fun took {end1-start1:.2f} seconds')
+    print(f'run 2 funs took {end2-end1:.2f} seconds')
 
 asyncio.run(main())
