dd933fad33e40e6e76718815f26b993aed8d3a69
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-01-13 14:14:38 +0200

    ud

---
+++ flaskr/tools/cv_2/homomorphic.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2
+
+# create black circle on white background for high pass filter call radius = 13
+# antialias GaussianBlur mask via blurring call blur = 47
+
+# read input and convert to grayscale
+img = cv2.imread('flaskr/tools/cv_2/holimorfic.png', cv2.IMREAD_GRAYSCALE)
+hh, ww = img.shape[:2]
+
+# take ln of image
+img_log = np.log(np.float64(img), dtype=np.float64)
+
+# do dft saving as complex output
+dft = np.fft.fft2(img_log, axes=(0,1))
+
+# apply shift of origin to center of image
+dft_shift = np.fft.fftshift(dft)
+
+# create black circle on white background for high pass filter
+#radius = 3
+radius = 13
+mask = np.zeros_like(img, dtype=np.float64)
+cy = mask.shape[0] // 2
+cx = mask.shape[1] // 2
+cv2.circle(mask, (cx,cy), radius, 1, -1)
+mask = 1 - mask
+
+# antialias mask via blurring
+# blur = 7
+blur = 47
+mask = cv2.GaussianBlur(mask, (blur, blur), 0)
+
+# apply mask to dft_shift
+dft_shift_filtered = np.multiply(dft_shift,mask)
+
+# shift origin from center to upper left corner
+back_ishift = np.fft.ifftshift(dft_shift_filtered)
+
+# do idft saving as complex
+img_back = np.fft.ifft2(back_ishift, axes=(0,1))
+
+# combine complex real and imaginary components to form (the magnitude for) the original image again
+img_back = np.abs(img_back)
+
+# apply exp to reverse the earlier log
+img_homomorphic = np.exp(img_back, dtype=np.float64)
+
+# scale result
+img_homomorphic = cv2.normalize(img_homomorphic, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+# write result to disk
+cv2.imwrite("person_dft_numpy_mask.png", (255*mask).astype(np.uint8))
+cv2.imwrite("person_dft_numpy_homomorphic.png", img_homomorphic)
+
+cv2.imshow("ORIGINAL", img)
+cv2.imshow("MASK", mask)
+cv2.imshow("FILTERED DFT/IFT ROUND TRIP", img_back)
+cv2.imshow("HOMOMORPHIC", img_homomorphic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
