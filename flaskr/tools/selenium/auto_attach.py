3bba4e2b7c869a8337f7633223a5c551ca5d26bc
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-01-26 01:59:38 +0200

    ud

--- flaskr/tools/selenium/auto_attach.py
+++ flaskr/tools/selenium/auto_attach.py
@@ -4,8 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import os
-
-# you need first to run the chrome driver in cmd (.\chromedriver.exe)
+from get_driver import connect_to_driver
+# you need first to run the chrome driver in cmd (.\chromedriver.exe --port=5000)
 
 # 1. open a driver
 # driver = webdriver.Chrome(options=options)
@@ -14,13 +14,17 @@ import os
 # session_id = driver.session_id            #'4e167f26-dc1d-4f51-a207-f761eaf73c31'
 # 3. Use these two parameter to connect to your driver.
 options = Options()
+options.add_argument("--start-maximized")
+# driver = webdriver.Chrome(options=options)
+port = 5000
+url = "http://127.0.0.1:" + str(port) # driver.command_executor._url
 options.add_experimental_option("detach", True)
-url = "http://localhost:9515"
+# url = "http://localhost:9515"
 driver = webdriver.Remote(command_executor=url, options=options)
 session_id = ""
 # 4. And you are connected to your driver again.
 dir_path = os.path.dirname(os.path.realpath(__file__))
-FILE_NAME = dir_path + "\\session_id.txt"
+FILE_NAME = dir_path + "\\session_id2.txt"
 with open(FILE_NAME, "r") as f:
     session_id = f.read()
 new_session_id = driver.session_id
@@ -40,7 +44,7 @@ print(driver.session_id)
 
 # testing
 driver.get("https://www.neuralnine.com/")
-driver.maximize_window()
+# driver.maximize_window()
 # driver.find_element(By.CSS_SELECTOR, "a[href='https://www.neuralnine.com/books/']").click()
 # driver.find_element(By.PARTIAL_LINK_TEXT, "Books").click()
 links = driver.find_elements(By.XPATH, "//a[@href]")
