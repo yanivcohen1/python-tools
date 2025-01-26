7bd84bedf56bf303b31b24d6a227428e79b83676
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-01-26 03:54:12 +0200

    ud

--- flaskr/tools/selenium/selenimue_auto.py
+++ flaskr/tools/selenium/selenimue_auto.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -5,9 +6,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 options = Options()
+options.add_argument("--start-maximized")
 options.add_experimental_option("detach", True)
+current_folder = os.path.dirname(os.path.abspath(__file__))
+chromedriver_path = os.path.join(current_folder, "chromedriver.exe")
+# chrome_install = ChromeDriverManager().install()
+# folder = os.path.dirname(chrome_install)
+# chromedriver_path = os.path.join(folder, "chromedriver.exe")
+service = Service(chromedriver_path)
 driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=options
+    service=service, options=options
 )
 driver.get("https://www.neuralnine.com/")
 driver.maximize_window()
