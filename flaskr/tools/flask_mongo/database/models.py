db2dac1a527ae1a0d2af597f53527b5b20f1bcbb
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-02-02 17:00:51 +0200

    ud

--- flaskr/tools/flask_mongo/database/models.py
+++ flaskr/tools/flask_mongo/database/models.py
@@ -1,21 +1,20 @@
 from enum import Enum
 from typing import List
+from flask_bcrypt import generate_password_hash, check_password_hash
 from flaskr.tools.flask_mongo.database.db import db
 from mongoengine import EmbeddedDocument, Document, StringField, ListField, EmbeddedDocumentField, ReferenceField, EmailField, EnumField, CASCADE, PULL
-from flask_bcrypt import generate_password_hash, check_password_hash
 
-# pylint: disable=no-member
-class Embed(db.EmbeddedDocument):
-    name: str = db.StringField(default='yan')
-    value: str = db.StringField(default='con')
+class Embed(EmbeddedDocument):
+    name: str = StringField(default='yan')
+    value: str = StringField(default='con')
 
 class Movie(db.Document):
-    name: str = db.StringField(required=True, unique=True)
-    casts: list[str] = db.ListField(db.StringField(), required=True)
-    genres: list[str]  = db.ListField(db.StringField(), required=True)
+    name: str = StringField(required=True, unique=True)
+    casts: list[str] = ListField(StringField(), required=True)
+    genres: list[str]  = ListField(StringField(), required=True)
     #find=Movie.objects(embeds__match={ "name": "xxx", "value": "xxx" }) # and query in embedded fields
-    embeds: list[Embed] = db.ListField(db.EmbeddedDocumentField(Embed))
-    added_by = db.ReferenceField('User')
+    embeds: list[Embed] = ListField(EmbeddedDocumentField(Embed))
+    added_by = ReferenceField('User')
 
 class Role(Enum):
     USER = 'user'
@@ -23,10 +22,10 @@ class Role(Enum):
     ADMIN = 'admin'
 
 class User(db.Document):
-    email: str = db.EmailField(required=True, unique=True)
-    password: str = db.StringField(required=True, min_length=6)
-    role: Role = db.EnumField(Role, default=Role.USER)
-    movies: list[Movie] = db.ListField(db.ReferenceField('Movie', reverse_delete_rule=db.PULL))
+    email: str = EmailField(required=True, unique=True)
+    password: str = StringField(required=True, min_length=6)
+    role: Role = EnumField(Role, default=Role.USER)
+    movies: list[Movie] = ListField(ReferenceField('Movie', reverse_delete_rule=PULL))
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
@@ -34,4 +33,4 @@ class User(db.Document):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
-User.register_delete_rule(Movie, 'added_by', db.CASCADE)
+User.register_delete_rule(Movie, 'added_by', CASCADE)
