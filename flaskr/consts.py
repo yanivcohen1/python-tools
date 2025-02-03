a03826b1c7b9b4ab8d6ec9bfa2830ff0cf1c1c28
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-02-03 19:28:10 +0200

    ud

--- flaskr/consts.py
+++ flaskr/consts.py
@@ -1,6 +1,6 @@
 class consts:
-    def PI(): return 3.14
-    def NAME(): return "yan"
+    def PI(): return 3.14 # pylint: disable=no-method-argument
+    def NAME(): return "yan" # pylint: disable=no-method-argument
 
 print("PI:", consts.PI(),
       ", my name:", consts.NAME())
