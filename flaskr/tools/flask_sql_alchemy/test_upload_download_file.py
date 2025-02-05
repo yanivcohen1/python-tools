f3961201c315690b1ec8c82eb74a8ea35d434585
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-02-05 20:18:28 +0200

    ud

--- flaskr/tools/flask_sql_alchemy/test_upload_download_file.py
+++ flaskr/tools/flask_sql_alchemy/test_upload_download_file.py
@@ -1,3 +1,5 @@
+import os
+import re
 import requests
 
 host = 'http://127.0.0.1:5000'
@@ -24,23 +26,25 @@ def upload():
 # Next, download the file
 def download():
     url = host + '/download/' + str(file_id)
-    file_path = r'C:\Users\yaniv\OneDrive\python-flask\flaskr\tools\flask_sql_alchemy\upload_download\hero_download.jpg'
+    file_path = r'C:\Users\yaniv\OneDrive\python-flask\flaskr\tools\flask_sql_alchemy\upload_download/'
 
     # Send a GET request to the URL
     response = requests.get(url)
-
-    # Save the content of the response to a file
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
-
-    # Print the status code
-    if response.status_code == 200:
-        print("downloaded file path:", file_path)
-    else:
+    if response.status_code != 200:
         print("Error downloading file")
+        return False
+    content_disposition = response.headers.get("Content-Disposition")
+    filename = re.findall('filename="?([^"]+)"?', content_disposition)
+    name, extension = os.path.splitext(filename[0])
+    file_full_download_path = file_path + name + "-download" + extension
+    print("downloaded file path:", file_full_download_path)
+    # Save the content of the response to a file
+    with open(file_full_download_path, 'wb') as file:
+        file.write(response.content)
+    return True
 
 
 if __name__ == '__main__':
-    file_id = upload()
-    # file_id = 1
+    # file_id = upload()
+    file_id = 1
     download()
