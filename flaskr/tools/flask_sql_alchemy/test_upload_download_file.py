e03954e73256e305e30610d0237391ea41c9429a
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-02-05 19:39:40 +0200

    ud

---
+++ flaskr/tools/flask_sql_alchemy/test_upload_download_file.py
@@ -0,0 +1,46 @@
+import requests
+
+host = 'http://127.0.0.1:5000'
+file_id = None
+# First, upload a file
+def upload():
+    url = host + '/upload'
+    file_path = r"C:\Users\yaniv\OneDrive\python-flask\flaskr\tools\flask_sql_alchemy\upload_download\hero.jpg"
+
+    # Open the file in binary mode and send it using a POST request
+    with open(file_path, 'rb') as file:
+        files = {'file': file}
+        response = requests.post(url, files=files)
+
+    # Print the response
+    if response.status_code == 200:
+        file_id = response.json().get("file_id")
+        print("upload file id:", file_id)
+    else:
+        print("Error uploading file")
+    return file_id
+
+
+# Next, download the file
+def download():
+    url = host + '/download/' + str(file_id)
+    file_path = r'C:\Users\yaniv\OneDrive\python-flask\flaskr\tools\flask_sql_alchemy\upload_download\hero_download.jpg'
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Save the content of the response to a file
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+    # Print the status code
+    if response.status_code == 200:
+        print("downloaded file path:", file_path)
+    else:
+        print("Error downloading file")
+
+
+if __name__ == '__main__':
+    file_id = upload()
+    # file_id = 1
+    download()
---
+++ flaskr/tools/flask_sql_alchemy/upload_download_file.py
@@ -0,0 +1,48 @@
+# import all libraires
+from io import BytesIO
+from flask import Flask, render_template, request, send_file, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize flask and create sqlite database
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://root:yanivc77@localhost/alchemy"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+# create datatable
+class Upload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary(length=(2**32)-1)) # MEDIUMBLOB up to 16M
+
+
+# Create index function for upload and return files
+@app.route("/upload", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        file = request.files["file"]
+        upload = Upload(filename=file.filename, data=file.read())
+        db.session.add(upload)
+        db.session.commit()
+        return jsonify({"Uploaded": file.filename, "file_id": upload.id}), 200
+    return "file not uploaded only post method", 401
+
+
+# create download function for download files
+@app.route("/download/<upload_id>")
+def download(upload_id):
+    upload = Upload.query.filter_by(id=upload_id).first()
+    return send_file(
+        BytesIO(upload.data), download_name=upload.filename, as_attachment=True
+    )
+
+
+if __name__ == "__main__":
+    # with app.app_context():
+    #     # Create the tables
+    #     db.create_all()
+
+    app.run(debug=True, port=5000)
