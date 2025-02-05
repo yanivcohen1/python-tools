a3c1b36281116784029a31e312bbf04e3c3944d9
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-02-05 21:52:43 +0200

    ud

--- flaskr/tools/pynput_hook_test.py
+++ flaskr/tools/pynput_hook_test.py
@@ -15,14 +15,14 @@ def on_activate():
     print("clipbord:", clipbord)
     typewrite(clipbord)
     # keyboard_c.type('HellOo1 World')
-    # keyboard_c.press(Key.space)
-    # keyboard_c.release(Key.space)
+    # keyboard_c.press(Key.backspace)
+    # keyboard_c.release(Key.backspace)
 
 def for_canonical(f):
     return lambda k: f(l.canonical(k))
 
 hotkey = keyboard.HotKey(
-    keyboard.HotKey.parse('<F8>'),
+    keyboard.HotKey.parse('<F8>'), # <ctrl>+<alt>+h
     on_activate)
 with keyboard.Listener(
         on_press=for_canonical(hotkey.press),
