d93ad6605fc7ecffd7899ce37c5a57ab6110845c
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-02-09 02:26:17 +0200

    ud

--- flaskr/tools/ai_chat/runOllamaChat.bat
+++ flaskr/tools/ai_chat/runOllamaChat.bat
@@ -1,3 +1,4 @@
-cmd /c start ollama_serve.bat
-cmd /c start http://localhost:8000/ollama_chat.html
+rem cmd /c start ollama_serve.bat
+cmd /c start cmd /c ollama serve
+cmd /c start http://localhost:8000/ollama_chat_all.html
 python -m http.server 8000
