828c21a0bb6d25170549de20655ce4ec14003bdc
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-02-12 09:18:36 +0200

    ud

--- flaskr/tools/load_test/locust_load.py
+++ flaskr/tools/load_test/locust_load.py
@@ -48,7 +48,7 @@ def run_locust():
         "--headless",
         "-u", str(num_users),
         "-r", str(spawn_rate),
-        "--run-time", "1m",  # Duration of the test
+        "--run-time", "1m",  # Duration of the test for timeout
         "--csv", "locust_report",  # Prefix for the CSV report files
         "--host", "http://your-api-url.com"  # Replace with your API URL
     ]
