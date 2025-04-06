d64ffcc1cefd540672a379b8fccf97c558ca75f9
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-04-06 22:13:38 +0300

    ud

--- flaskr/tools/ai_chat/langchain_ai/main/image_ollama.py
+++ flaskr/tools/ai_chat/langchain_ai/main/image_ollama.py
@@ -13,10 +13,10 @@ def encode_image_to_base64(image_path):
 current_path = os.path.dirname(os.path.abspath(__file__))
 images_path = current_path + "/../content/images/"
 
-image_name = "cube.png" # "transparency_demonstration.png"# "smail_face.png"#  # "objectdetection.png"
+image_name = "pic1.jpg" # "transparency_demonstration.png"# "smail_face.png"#  # "objectdetection.png"
 model_name = "gemma3:4b" # "llava:7b" #
 image_url = images_path + image_name
-image_base64 = encode_image_to_base64(image_url)
+# image_base64 = encode_image_to_base64(image_url)
 
 # Use Ollama with llava
 model = OllamaLLM(model=model_name, temperature=0.8) #  phi4-mini:3.8b
@@ -24,19 +24,20 @@ model = OllamaLLM(model=model_name, temperature=0.8) #  phi4-mini:3.8b
 chat_history = ""
 while True:
     print("\n\n-------------------------------")
+    pic_name = input("pic name (q to quit): ")
+    if pic_name == "": pic_name = "cow.jpg"
+    if pic_name == "q": break
     question = input("Ask your question (q to quit): ")
-    if question == "":
-        question = "What is in this image?  answer in few sentences"
+    if question == "": question = "What is in this image?  answer in few sentences"
+    if question == "q": break
     print("\n")
-    if question == "q":
-        break
     # Set up the message with image and text prompt
     messages = [
         HumanMessage(
             content=[
                 {"type": "text", "text": question},
                 {"type": "text", "text": "Previous conversation: " + chat_history},
-                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{image_base64}"}} # image_url}
+                {"type": "image_url", "image_url": images_path + pic_name} # {"url": f"data:image/png;base64,{image_base64}"}} # image_url}
             ]
         )
     ]
