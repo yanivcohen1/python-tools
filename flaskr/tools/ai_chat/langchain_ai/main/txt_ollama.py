338483c07b7a7dc1684c1a2768e2efed70b56e1f
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-04-07 11:48:03 +0300

    ud

---
+++ flaskr/tools/ai_chat/langchain_ai/main/txt_ollama.py
@@ -0,0 +1,38 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from txt_vector import ask_txt
+
+model = OllamaLLM(model="deepseek-coder-v2:16b", temperature=0.8) # gemma3:4b phi4-mini:3.8b
+
+pdf ="alices_adventures_in_wonderland.html"
+
+template = """
+You are an exeprt in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to answer: {question}
+
+Previous conversation: {chat_history}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+chat_history = ""
+while True:
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    if question == "":
+        question = "who is Alice? answer in few sentences"
+    print("\n")
+    if question == "q":
+        break
+
+    reviews = ask_txt(pdf).invoke(question) # returtn 60 chanks is 10% of chanks, 8 pages of 80 pages
+    # result = chain.invoke({"reviews": reviews, "question": question})
+    for chunk in chain.stream({"reviews": reviews, "question": question, "chat_history": chat_history}):
+        if chunk is not None:
+            msg = chunk
+            # chat_history += msg
+            print(msg, end="")
+    chat_history += question
---
+++ flaskr/tools/ai_chat/langchain_ai/main/txt_vector.py
@@ -0,0 +1,127 @@
+import os
+# from langchain.chains.combine_documents import create_stuff_documents_chain
+# from langchain.chains.retrieval import create_retrieval_chain
+# from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import PDFPlumberLoader, TextLoader
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_chroma import Chroma
+from langchain_ollama.llms import OllamaLLM
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+
+db_location = "./chrome_langchain_db"
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+docs_path = current_path + "/../content/docs/"
+
+cached_llm = OllamaLLM(model="mxbai-embed-large:335m")
+
+embedding = FastEmbedEmbeddings()
+
+collection_name = "alice_pdf"
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=384, chunk_overlap=80, length_function=len, is_separator_regex=False
+)
+
+collection_name="restaurant_reviews"
+
+def ask_ai(query: str):
+
+    print(f"query: {query}")
+
+    response = cached_llm.invoke(query)
+
+    print(response)
+
+    response_answer = {"answer": response}
+    return response_answer
+
+
+def ask_txt(file_name: str):
+    collection_name = file_name.replace(".", "_")
+
+    # Loading vector store
+    vector_store = Chroma(persist_directory=db_location,
+                          embedding_function=embedding, collection_name=collection_name)
+    chanks_len = get_chanks_len(collection_name)
+    chanks_seg = chanks_len//10+1
+    print(f"chanks_seg: {chanks_seg}")
+    retriever = vector_store.as_retriever(
+        search_kwargs={"k": chanks_seg} # 10% of chanks
+    )
+
+    return retriever
+
+
+def txt_to_vector(file_name: str):
+    save_file = docs_path + file_name
+    print(f"filename: {file_name}")
+
+    loader = TextLoader(save_file, encoding='utf-8')
+    docs = loader.load_and_split()
+    print(f"docs len={len(docs)}")
+    collection_name = file_name.replace(".", "_")
+
+    collections = get_collection_names()
+    collection_exist = True if collection_name in collections else False
+
+    if collection_exist:
+        print(f"collection {collection_name} already exist")
+        del_collection(collection_name)
+        # return False
+
+    chunks = text_splitter.split_documents(docs)
+    print(f"chunks len={len(chunks)}")
+
+    ids = []
+    for chunk in chunks:
+        ids.append(chunk.id)
+
+    vector_store = Chroma(
+        collection_name=collection_name,
+        persist_directory=db_location,
+        embedding_function=embedding
+    )
+
+    # save chunks(docs) to collection_name
+    vector_store.add_documents(documents=chunks, ids=ids)
+
+    # vector_store.persist()
+
+    response = {
+        "status": "Successfully Uploaded",
+        "filename": file_name,
+        "doc_len": len(docs),
+        "chunks": len(chunks),
+    }
+    print(f"response: {response}")
+    return response
+
+def get_collection_names():
+    vector_store = Chroma(persist_directory=db_location) # , embedding_function=embedding
+    collections = vector_store._client.list_collections()
+    print(f"collections: {collections}")
+    return collections
+
+def del_collection(collection_name: str):
+    vector_store = Chroma(persist_directory=db_location) # , embedding_function=embedding
+    vector_store._client.delete_collection(collection_name)
+    print(f"deleted collection: {collection_name}")
+    return True
+
+def get_chanks_len(collection_name: str):
+    vector_store = Chroma(persist_directory=db_location, embedding_function=embedding, collection_name=collection_name)
+    collection = vector_store._client.get_collection(collection_name)
+    all_ids = collection.get()
+    num_docs = len(all_ids["ids"])
+    print(f"num_docs(chanks): {num_docs}")
+    return num_docs
+
+if __name__ == "__main__":
+    # get_collection_names()
+    get_chanks_len("alices_adventures_in_wonderland_html") # 573
+    # del_collection("alices_adventures_in_wonderland_html")
+    # txt_to_vector("alices_adventures_in_wonderland.html")
+    get_collection_names()
