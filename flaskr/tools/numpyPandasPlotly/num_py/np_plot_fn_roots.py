c346cbabf57b3bbaa70871facdb03bd5691d1109
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-04-09 00:30:48 +0300

    ud

--- flaskr/tools/numpyPandasPlotly/num_py/np_plot_fn_roots.py
+++ flaskr/tools/numpyPandasPlotly/num_py/np_plot_fn_roots.py
@@ -56,7 +56,7 @@ for i in range(2):
         if y2 - y1 == 0:
             continue
 
-        # Linear interpolation formula
+        # Linear interpolation formula (x_zero - x1) / (0 - y1) = (x2 - x1) / (y2 - y1)
         x_zero = x1 + (0 - y1) * (x2 - x1) / (y2 - y1)
         x_crossings.append(x_zero)
 
