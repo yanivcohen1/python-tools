4f2517fb587e600bb077998d69d2bab63aec341d
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-04-09 10:34:29 +0300

    ud

--- flaskr/tools/ai_chat/langchain_ai/main/txt_vector.py
+++ flaskr/tools/ai_chat/langchain_ai/main/txt_vector.py
@@ -21,7 +21,7 @@ embedding = FastEmbedEmbeddings()
 
 collection_name = "alice_pdf"
 
-text_splitter = RecursiveCharacterTextSplitter(
+text_splitter = RecursiveCharacterTextSplitter( # 80 characters overlap between chunks (for not cut sentences)
     chunk_size=384, chunk_overlap=80, length_function=len, is_separator_regex=False
 )
 
