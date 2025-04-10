3fb9bc6931dcf65077f2335d791497ffd2006c77
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-04-10 12:41:54 +0300

    ud

---
+++ flaskr/tools/ai_chat/langchain_ai/RGA/image_open_ai.py
@@ -0,0 +1,53 @@
+import base64
+import os
+from io import BytesIO
+from PIL import Image
+import httpx
+from langchain_core.messages import HumanMessage
+# Import the updated Ollama model from the new package
+from langchain_ollama.llms import OllamaLLM
+from langchain_openai import ChatOpenAI
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+images_path = os.path.abspath(current_path + "/../content/images/")
+
+model = ChatOpenAI(
+        api_key="your_api_key_here",  # Replace with your actual API key
+        temperature=0.8,
+        model="gemma3:4b",  # or "gpt-3.5-turbo"
+        base_url="http://localhost:11434/v1"
+        # stream_usage=True,
+        # callback_manager=callback_manager
+)
+
+def convert_to_base64(image_path: str) -> str:
+    """
+    Opens an image file and converts it to a base64-encoded string.
+    """
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+    return encoded_string
+
+image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+pic_name = 'cow.jpg'  # "cow.png"
+image_data = convert_to_base64(f"{images_path}/{pic_name}")
+image_data_url = base64.b64encode(httpx.get(image_url).content).decode("utf-8")
+message_embed = HumanMessage(
+    content=[
+        {"type": "text", "text": "describe the weather in this image"},
+        {
+            "type": "image_url",
+            "image_url": {"url": f"data:image/jpeg;base64,{image_data}"}, # image_data
+        },
+    ],
+)
+
+message_url = HumanMessage(
+    content=[
+        {"type": "text", "text": "describe the weather in this image"},
+        {"type": "image_url", "image_url": {"url": image_url}},
+    ],
+)
+
+response = model.invoke([message_embed])
+print(response.content)
