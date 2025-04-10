8df2b462ab87eccaaa44714aec34880aac29f9a6
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-04-10 08:52:39 +0300

    ud

--- flaskr/tools/ai_chat/langchain_ai/RGA/image_ollama.py
+++ flaskr/tools/ai_chat/langchain_ai/RGA/image_ollama.py
@@ -11,20 +11,22 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 images_path = os.path.abspath(current_path + "/../content/images/")
 model = OllamaLLM(model=model_name, temperature=0.8)  #  phi4-mini:3.8b
 
-# def convert_to_base64(image_path: str) -> str:
-#     pil_image = Image.open(image_path)
-#     buffered = BytesIO()
-#     pil_image.save(buffered, format="JPEG")  # You can change the format if needed
-#     img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-#     return img_str
-
 def convert_to_base64(image_path: str) -> str:
-    """
-    Opens an image file and converts it to a base64-encoded string.
-    """
-    with open(image_path, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
-    return encoded_string
+    pil_image = Image.open(image_path)
+    buffered = BytesIO()
+    if pil_image.mode == 'RGBA':
+        pil_image = pil_image.convert('RGB')  # Drop the alpha channel
+    pil_image.save(buffered, format="JPEG")  # You can change the format if needed
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    return img_str
+
+# def convert_to_base64(image_path: str) -> str:
+#     """
+#     Opens an image file and converts it to a base64-encoded string.
+#     """
+#     with open(image_path, "rb") as image_file:
+#         encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+#     return encoded_string
 
 chat_history = ""
 while True:
