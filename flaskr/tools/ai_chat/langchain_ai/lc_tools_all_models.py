efb41751878f489ce004db4203ff0ff622d84d13
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-04-19 20:39:43 +0300

    ud

--- flaskr/tools/ai_chat/langchain_ai/lc_tools_all_models.py
+++ flaskr/tools/ai_chat/langchain_ai/lc_tools_all_models.py
@@ -1,3 +1,4 @@
+import json
 from langchain.agents import Tool, initialize_agent
 from langchain.agents.agent_types import AgentType
 from langchain_ollama.llms import OllamaLLM
@@ -13,7 +14,7 @@ def simple_calculator(expr: str) -> str:
     except Exception as e:
         return f"Error: {e}, expr: {expr}"
 
-def weather_tool(city: str) -> str:
+def weather_tool(city: str, format: str = "Celsius") -> str:
     fake_data = {
         "new york": "15°C, cloudy",
         "london": "10°C, rainy",
@@ -21,16 +22,30 @@ def weather_tool(city: str) -> str:
     }
     return fake_data.get(city.lower(), "Weather data not available.")
 
+def weather_tool_input_parser(json_str: str) -> str:
+    try:
+        data = json.loads(json_str)
+        city = data.get("city")
+        format = data.get("format", "Celsius")
+        return weather_tool(city, format)
+    except Exception as e:
+        return f"Invalid input format. Please provide JSON like {{'city': 'Paris', 'format': 'Fahrenheit'}}. Error: {e}, json is {json_str}"
+
 tools = [
     Tool(
         name="Calculator",
         func=simple_calculator,
         description="Useful for math operations. Input should be a valid Python math expression."
     ),
+    # Tool(
+    #     name="WeatherInfo",
+    #     func=weather_tool,
+    #     description="Gives current weather for a city in Celsius. Input should be a city name."
+    # ),
     Tool(
-        name="WeatherInfo",
-        func=weather_tool,
-        description="Gives current weather for a city in Celsius. Input should be a city name."
+        name="WeatherInfoJSON",
+        func=weather_tool_input_parser,
+        description="Gives current weather for a city. Input should be a JSON string with 'city' and optional 'format' (Celsius or Fahrenheit)."
     )
 ]
 
@@ -48,7 +63,7 @@ while True:
     print("\n-------------------------------")
     try:
         # response = agent.invoke("what is a cat and What is the weather in Cairo use WeatherInfo and what's 42 divided by 7 use Calculator?")
-        response = agent.invoke("what is a cat and What is the weather in Cairo and convert it to Fahrenheit?") # what's 42 divided by 7
+        response = agent.invoke("what is a cat and What is the weather in Cairo in Celsius and convert it to Fahrenheit?") # what's 42 divided by 7
         print(response['output'])
     except Exception as e:
         continue
