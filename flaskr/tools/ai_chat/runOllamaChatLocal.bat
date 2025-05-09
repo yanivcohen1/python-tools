a1ff55a4c9bab9377b7b0603244e420f1faccfda
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-05-09 17:34:08 +0300

    ud

--- flaskr/tools/ai_chat/runOllamaChatLocal.bat
+++ flaskr/tools/ai_chat/runOllamaChatLocal.bat
@@ -6,13 +6,13 @@ REM cmd /c start http://localhost:8000/ollama_chat_local.html
 REM python -m http.server 8000
 
 :: Start the local HTML page
-start "" "http://localhost:9000/ollama_chat_local.html"
+start "" "http://localhost:8000/ollama_chat_local.html"
 
 :: Start the proxy HTML page
 start "" "http://localhost:9000/ollama_chat_linux.html"
 
 :: Start the Python HTTP server on port 9000
-py -m http.server 9000
+py -m http.server 8000
 
 :: Keep the Command Prompt window open
 @REM echo The Python HTTP server is running. Press CTRL+C to stop.
