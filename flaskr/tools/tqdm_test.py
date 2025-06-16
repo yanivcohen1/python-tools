3a317bda6650c283991e4d38edd8f845e71231fb
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-06-16 17:32:33 +0300

    ud

--- flaskr/tools/tqdm_test.py
+++ flaskr/tools/tqdm_test.py
@@ -1,5 +1,6 @@
 from tqdm.auto import tqdm
 import time
 
+# The code snippet demonstrates how to use the `tqdm` library to create a progress bar in Python.
 for i in tqdm(range(10)):
-    time.sleep(0.1)
+    time.sleep(0.3)  # Simulate some work being done
